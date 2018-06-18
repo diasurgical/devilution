125defa8905fe2920a62c8e0c25884aec7c3da17
galaxyhaxz <andrew-haxz@hotmail.com>
2018-06-18 18:57:24 -0500

    Add files via upload

--- Source/spells.h
+++ Source/spells.h
@@ -1,13 +1,4 @@
-/*
- * UNPUBLISHED -- Rights  reserved  under  the  copyright  laws  of the
- * United States.  Use  of a copyright notice is precautionary only and
- * does not imply publication or disclosure.
- *
- * THIS DOCUMENTATION CONTAINS CONFIDENTIAL AND PROPRIETARY INFORMATION
- * OF    BLIZZARD   ENTERTAINMENT.    ANY   DUPLICATION,  MODIFICATION,
- * DISTRIBUTION, OR DISCLOSURE IS STRICTLY PROHIBITED WITHOUT THE PRIOR
- * EXPRESS WRITTEN PERMISSION OF BLIZZARD ENTERTAINMENT.
- */
+//HEADER_GOES_HERE
 
 int __fastcall GetManaAmount(int id, int sn);
 void __fastcall UseMana(int id, int sn);
