5b6f565e4e9100fd0a257865790fdd131712e03a
galaxyhaxz <galaxy-haxz@hotmail.com>
2018-06-18 19:16:32 -0500

    Remove the "fear factor" (#21)
    Snakes, spiders, Demons, what scares you?

--- Source/codec.h
+++ Source/codec.h
@@ -1,13 +1,4 @@
-/*
- * UNPUBLISHED -- Rights  reserved  under  the  copyright  laws  of the
- * United States.  Use  of a copyright notice is precautionary only and
- * does not imply publication or disclosure.
- *
- * THIS DOCUMENTATION CONTAINS CONFIDENTIAL AND PROPRIETARY INFORMATION
- * OF    BLIZZARD   ENTERTAINMENT.    ANY   DUPLICATION,  MODIFICATION,
- * DISTRIBUTION, OR DISCLOSURE IS STRICTLY PROHIBITED WITHOUT THE PRIOR
- * EXPRESS WRITTEN PERMISSION OF BLIZZARD ENTERTAINMENT.
- */
+//HEADER_GOES_HERE
 
 int __fastcall codec_decode(void *pbSrcDst, int size, char *pszPassword);
 void __fastcall codec_init_key(int unused, char *pszPassword);
--- Source/drlg_l3.h
+++ Source/drlg_l3.h
@@ -1,13 +1,4 @@
-/*
- * UNPUBLISHED -- Rights  reserved  under  the  copyright  laws  of the
- * United States.  Use  of a copyright notice is precautionary only and
- * does not imply publication or disclosure.
- *
- * THIS DOCUMENTATION CONTAINS CONFIDENTIAL AND PROPRIETARY INFORMATION
- * OF    BLIZZARD   ENTERTAINMENT.    ANY   DUPLICATION,  MODIFICATION,
- * DISTRIBUTION, OR DISCLOSURE IS STRICTLY PROHIBITED WITHOUT THE PRIOR
- * EXPRESS WRITTEN PERMISSION OF BLIZZARD ENTERTAINMENT.
- */
+//HEADER_GOES_HERE
 
 //drlg_l3
 extern char lavapool; // weak
