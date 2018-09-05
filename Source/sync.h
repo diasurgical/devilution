2fba739883d0768d1d354f5403120c3b78b660b7
galaxyhaxz <galaxy-haxz@hotmail.com>
2018-09-05 06:32:26 -0500

    Fix TSyncHeader and sync.cpp (#192)

--- Source/sync.h
+++ Source/sync.h
@@ -3,17 +3,17 @@
 #define __SYNC_H__
 
 extern short sync_word_6AA708[MAXMONSTERS];
-extern int dword_6AA898; // weak
+extern int syncmonsters; // weak
 extern short sync_word_6AA89C[MAXMONSTERS];
-extern int dword_6AAA2C[2];
+extern int syncitems;
 extern int sgnSyncPInv; // weak
 
 int __fastcall sync_all_monsters(TSyncHeader *packet, int size);
 void __cdecl sync_one_monster();
 int __fastcall sync_monster_active(TSyncMonster *packet);
-int __fastcall sync_monster_pos(TSyncMonster *packet, int mon_id);
+void __fastcall sync_monster_pos(TSyncMonster *packet, int mon_id);
 int __fastcall sync_monster_active2(TSyncMonster *packet);
-char __fastcall SyncPlrInv(TSyncHeader *pItem);
+void __fastcall SyncPlrInv(TSyncHeader *pSync);
 int __fastcall SyncData(int pnum, TSyncHeader *packet);
 void __fastcall sync_monster_data(int pnum, TSyncMonster *packet);
 void __cdecl sync_clear_pkt();
