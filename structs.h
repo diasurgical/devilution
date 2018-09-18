2f7f7d055e690b87fe61a25e3d8f930e3944c511
Dennis Duda <git@seri.tools>
2018-09-18 23:30:25 +0200

    Switched all remaining uses of `UINT64` to `unsigned __int64` to make it compilable on <=VC5

--- structs.h
+++ structs.h
@@ -213,15 +213,15 @@ struct PlayerStruct
 	char _pSBkSplType;
 	char _pSplLvl[64];
 	union {
-		__int64 _pMemSpells64;
+		unsigned __int64 _pMemSpells64;
 		int _pMemSpells[2];
 	};
 	union {
-		__int64 _pAblSpells64;
+		unsigned __int64 _pAblSpells64;
 		int _pAblSpells[2];
 	};
 	union {
-		__int64 _pScrlSpells64;
+		unsigned __int64 _pScrlSpells64;
 		int _pScrlSpells[2];
 	};
 	int _pSpellFlags;
@@ -322,7 +322,7 @@ struct PlayerStruct
 	int _pIBonusAC;
 	int _pIBonusDamMod;
 	union {
-		__int64 _pISpells64;
+		unsigned __int64 _pISpells64;
 		int _pISpells[2];
 	};
 	int _pIFlags;
