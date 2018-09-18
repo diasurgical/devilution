523b1d597450b7ffcb451cb1e7678d06bab7b827
galaxyhaxz <galaxy-haxz@hotmail.com>
2018-09-18 04:01:47 -0500

    Update storm.cpp

--- 3rdParty/Storm/Source/storm.cpp
+++ 3rdParty/Storm/Source/storm.cpp
@@ -243,5 +243,7 @@ void __stdcall SDlgBeginPaint(HWND hWnd, char *a2) rVoid;
 void __stdcall SDlgEndPaint(HWND hWnd, char *a2) rVoid;
 void __stdcall SDlgSetSystemCursor(BYTE *a1, BYTE *a2, int *a3, int a4) rVoid;
 void __stdcall SDlgSetCursor(HWND hWnd, HCURSOR a2, int a3, int *a4) rVoid;
-BOOL __stdcall SDlgSetTimer(int a1, int a2, int a3, void (__stdcall *a4)(int)) rBool;
+BOOL __stdcall SDlgSetTimer(int a1, int a2, int a3, void (__stdcall *a4)(int, int, int, int)) rBool;
 BOOL __stdcall SDlgKillTimer(int a1, int a2) rBool;
+BOOL __stdcall SDlgDrawBitmap(HWND hWnd, int a2, int a3, int a4, int a5, int a6, int a7) rBool;
+BOOL __stdcall SDlgDialogBoxParam(HINSTANCE hInst, char *szDialog, int a3, WNDPROC func, int a5) rBool;
