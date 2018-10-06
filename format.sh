892e84136e6e5646870ea9e96c0fb7944b45cec4
Robin Eklind <rnd0x00@gmail.com>
2018-10-06 20:20:18 +0200

    Add Devilution specific config values for clang-format.
    * AlignTrailingComments: true
    * AllowShortBlocksOnASingleLine: true
    * AllowShortFunctionsOnASingleLine: None
    * PointerAlignment: Right
    
    Note, currently, the format.sh script uses the config file
    for .cpp files. The `AlignConsecutiveAssignments: true` setting
    is specifically added for .h files.
    
    I could not find a way to specify what config file to use
    for clang-format. So, for now, the config file is used for
    .cpp.

--- format.sh
+++ format.sh
@@ -4,7 +4,7 @@ CPP_DONE="Source/doom.cpp Source/movie.cpp Source/pfile.cpp Source/player.cpp So
 
 for f in $CPP_DONE; do
 	echo "Formatting $f"
-	clang-format -style="{BasedOnStyle: webkit, AlignTrailingComments: true, AllowShortBlocksOnASingleLine: true, AllowShortFunctionsOnASingleLine: None, PointerAlignment: Right}" -i $f
+	clang-format -style=file -i $f
 done
 
 H_DONE="enums.h structs.h"
