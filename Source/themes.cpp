7c91a633c20f1c0a571b8a4d4c7dc4302e70588c
Robin Eklind <rnd0x00@gmail.com>
2018-11-01 00:48:19 +0100

    Theme_Shrine bin exact (already was)

--- Source/themes.cpp
+++ Source/themes.cpp
@@ -734,9 +734,13 @@ void __fastcall Theme_Barrel(int t)
 	PlaceThemeMonsts(t, monstrnd[leveltype - 1]);
 }
 
+// Theme_Shrine initializes the shrine theme.
+//
+// Parameters:
+//    - t: theme number (index into themes array).
 void __fastcall Theme_Shrine(int t)
 {
-	char monstrnd[4]; // [esp+3h] [ebp-5h]
+	char monstrnd[4];
 
 	monstrnd[0] = 6;
 	monstrnd[1] = 6;
