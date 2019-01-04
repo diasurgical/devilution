8af858bc797f80c6f18fa44f2ec3fb962e2c62a8
Anders Jenbo <anders@jenbo.dk>
2019-01-04 22:43:07 +0100

    Make inline statments C compatible

--- Source/_asm.cpp
+++ Source/_asm.cpp
@@ -1,14 +1,14 @@
-static inline void asm_cel_light_edge(unsigned char w, BYTE *tbl, BYTE *&dst, BYTE *&src);
-static inline void asm_cel_light_square(unsigned char w, BYTE *tbl, BYTE *&dst, BYTE *&src);
-static inline void asm_trans_light_cel_0_2(unsigned char w, BYTE *tbl, BYTE *&dst, BYTE *&src);
-static inline void asm_trans_light_edge_0_2(unsigned char w, BYTE *tbl, BYTE *&dst, BYTE *&src);
-static inline void asm_trans_light_square_0_2(unsigned char w, BYTE *tbl, BYTE *&dst, BYTE *&src);
-static inline void asm_trans_light_cel_1_3(unsigned char w, BYTE *tbl, BYTE *&dst, BYTE *&src);
-static inline void asm_trans_light_edge_1_3(unsigned char w, BYTE *tbl, BYTE *&dst, BYTE *&src);
-static inline void asm_trans_light_square_1_3(unsigned char w, BYTE *tbl, BYTE *&dst, BYTE *&src);
-static inline unsigned int asm_trans_light_mask(unsigned char w, BYTE *tbl, BYTE *&dst, BYTE *&src, unsigned int mask);
+static __inline void asm_cel_light_edge(unsigned char w, BYTE *tbl, BYTE *&dst, BYTE *&src);
+static __inline void asm_cel_light_square(unsigned char w, BYTE *tbl, BYTE *&dst, BYTE *&src);
+static __inline void asm_trans_light_cel_0_2(unsigned char w, BYTE *tbl, BYTE *&dst, BYTE *&src);
+static __inline void asm_trans_light_edge_0_2(unsigned char w, BYTE *tbl, BYTE *&dst, BYTE *&src);
+static __inline void asm_trans_light_square_0_2(unsigned char w, BYTE *tbl, BYTE *&dst, BYTE *&src);
+static __inline void asm_trans_light_cel_1_3(unsigned char w, BYTE *tbl, BYTE *&dst, BYTE *&src);
+static __inline void asm_trans_light_edge_1_3(unsigned char w, BYTE *tbl, BYTE *&dst, BYTE *&src);
+static __inline void asm_trans_light_square_1_3(unsigned char w, BYTE *tbl, BYTE *&dst, BYTE *&src);
+static __inline unsigned int asm_trans_light_mask(unsigned char w, BYTE *tbl, BYTE *&dst, BYTE *&src, unsigned int mask);
 
-static inline void asm_cel_light_edge(unsigned char w, BYTE *tbl, BYTE *&dst, BYTE *&src)
+static __inline void asm_cel_light_edge(unsigned char w, BYTE *tbl, BYTE *&dst, BYTE *&src)
 {
 	unsigned char l = w >> 1;
 
@@ -27,7 +27,7 @@ static inline void asm_cel_light_edge(unsigned char w, BYTE *tbl, BYTE *&dst, BY
 	asm_cel_light_square(l >> 1, tbl, dst, src);
 }
 
-static inline void asm_cel_light_square(unsigned char w, BYTE *tbl, BYTE *&dst, BYTE *&src)
+static __inline void asm_cel_light_square(unsigned char w, BYTE *tbl, BYTE *&dst, BYTE *&src)
 {
 	for (; w; --w) {
 		dst[0] = tbl[src[0]];
@@ -39,7 +39,7 @@ static inline void asm_cel_light_square(unsigned char w, BYTE *tbl, BYTE *&dst, 
 	}
 }
 
-static inline void asm_trans_light_cel_0_2(unsigned char w, BYTE *tbl, BYTE *&dst, BYTE *&src)
+static __inline void asm_trans_light_cel_0_2(unsigned char w, BYTE *tbl, BYTE *&dst, BYTE *&src)
 {
 	if (!(w & 1)) {
 		asm_trans_light_edge_1_3(w >> 1, tbl, dst, src);
@@ -50,7 +50,7 @@ static inline void asm_trans_light_cel_0_2(unsigned char w, BYTE *tbl, BYTE *&ds
 	}
 }
 
-static inline void asm_trans_light_edge_0_2(unsigned char w, BYTE *tbl, BYTE *&dst, BYTE *&src)
+static __inline void asm_trans_light_edge_0_2(unsigned char w, BYTE *tbl, BYTE *&dst, BYTE *&src)
 {
 	unsigned char l = w >> 1;
 
@@ -64,7 +64,7 @@ static inline void asm_trans_light_edge_0_2(unsigned char w, BYTE *tbl, BYTE *&d
 	}
 }
 
-static inline void asm_trans_light_square_0_2(unsigned char w, BYTE *tbl, BYTE *&dst, BYTE *&src)
+static __inline void asm_trans_light_square_0_2(unsigned char w, BYTE *tbl, BYTE *&dst, BYTE *&src)
 {
 	for (; w; --w) {
 		dst[0] = tbl[src[0]];
@@ -74,7 +74,7 @@ static inline void asm_trans_light_square_0_2(unsigned char w, BYTE *tbl, BYTE *
 	}
 }
 
-static inline void asm_trans_light_cel_1_3(unsigned char w, BYTE *tbl, BYTE *&dst, BYTE *&src)
+static __inline void asm_trans_light_cel_1_3(unsigned char w, BYTE *tbl, BYTE *&dst, BYTE *&src)
 {
 	if (!(w & 1)) {
 		asm_trans_light_edge_0_2(w >> 1, tbl, dst, src);
@@ -86,7 +86,7 @@ static inline void asm_trans_light_cel_1_3(unsigned char w, BYTE *tbl, BYTE *&ds
 	}
 }
 
-static inline void asm_trans_light_edge_1_3(unsigned char w, BYTE *tbl, BYTE *&dst, BYTE *&src)
+static __inline void asm_trans_light_edge_1_3(unsigned char w, BYTE *tbl, BYTE *&dst, BYTE *&src)
 {
 	unsigned char l = w >> 1;
 
@@ -100,7 +100,7 @@ static inline void asm_trans_light_edge_1_3(unsigned char w, BYTE *tbl, BYTE *&d
 	}
 }
 
-static inline void asm_trans_light_square_1_3(unsigned char w, BYTE *tbl, BYTE *&dst, BYTE *&src)
+static __inline void asm_trans_light_square_1_3(unsigned char w, BYTE *tbl, BYTE *&dst, BYTE *&src)
 {
 	for (; w; --w) {
 		dst[1] = tbl[src[1]];
@@ -110,7 +110,7 @@ static inline void asm_trans_light_square_1_3(unsigned char w, BYTE *tbl, BYTE *
 	}
 }
 
-static inline unsigned int asm_trans_light_mask(unsigned char w, BYTE *tbl, BYTE *&dst, BYTE *&src, unsigned int mask)
+static __inline unsigned int asm_trans_light_mask(unsigned char w, BYTE *tbl, BYTE *&dst, BYTE *&src, unsigned int mask)
 {
 	for (; w; --w, src++, dst++, mask *= 2) {
 		if (mask & 0x80000000)
