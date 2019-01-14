d588ec2305dc15dc8f3c45f17f3e36ae960e07b2
Anders Jenbo <anders@jenbo.dk>
2019-01-14 03:19:50 +0100

    Clean up dthread.cpp

--- Source/dthread.h
+++ Source/dthread.h
@@ -4,8 +4,8 @@
 
 extern unsigned int glpDThreadId; // idb
 extern TMegaPkt *sgpInfoHead;     /* may not be right struct */
-extern char byte_52A508;          // weak
-extern HANDLE sghWorkToDoEvent;   // idb
+extern BOOLEAN dthread_running;
+extern HANDLE sghWorkToDoEvent;
 
 void __cdecl dthread_init_mutex();
 void __cdecl dthread_cleanup_mutex_atexit();
@@ -13,7 +13,7 @@ void __cdecl dthread_cleanup_mutex(void);
 void __fastcall dthread_remove_player(int pnum);
 void __fastcall dthread_send_delta(int pnum, char cmd, void *pbSrc, int dwLen);
 void __cdecl dthread_start();
-unsigned int __stdcall dthread_handler(void *a1);
+unsigned int __stdcall dthread_handler(void *unused);
 void __cdecl dthread_cleanup();
 
 /* data */
