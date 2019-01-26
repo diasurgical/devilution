9d44169ac7f7e265e92dfc0a85bd5f59edf13ce1
Anders Jenbo <anders@jenbo.dk>
2019-01-26 17:30:09 +0100

    multi_upgrade (#583)
    * Clean up multi_upgrade
    
    * Correct variable names
    
    * Correct variable types

--- Source/multi.cpp
+++ Source/multi.cpp
@@ -7,7 +7,7 @@ TBuffer sgHiPriBuf;
 char szPlayerDescript[128];
 short sgwPackPlrOffsetTbl[MAX_PLRS];
 PkPlayerStruct netplr[MAX_PLRS];
-char sgbPlayerTurnBitTbl[MAX_PLRS];
+BYTE sgbPlayerTurnBitTbl[MAX_PLRS];
 char sgbPlayerLeftGameTbl[MAX_PLRS];
 int sgbSentThisCycle; // idb
 int dword_678628;     // weak
@@ -888,7 +888,7 @@ int __fastcall multi_init_single(_SNETPROGRAMDATA *client_info, _SNETPLAYERDATA 
 }
 // 679660: using guessed type char gbMaxPlayers;
 
-int __fastcall multi_init_multi(_SNETPROGRAMDATA *client_info, _SNETPLAYERDATA *user_info, _SNETUIDATA *ui_info, int *a4)
+BOOL __fastcall multi_init_multi(_SNETPROGRAMDATA *client_info, _SNETPLAYERDATA *user_info, _SNETUIDATA *ui_info, int *pfExitProgram)
 {
 	_SNETPLAYERDATA *v4; // ebx
 	signed int i;        // edi
@@ -902,7 +902,7 @@ int __fastcall multi_init_multi(_SNETPROGRAMDATA *client_info, _SNETPLAYERDATA *
 		type = 0;
 		if (byte_678640) {
 			if (!UiSelectProvider(0, (_SNETPROGRAMDATA *)a2, v4, ui_info, &fileinfo, &type)
-			    && (!i || SErrGetLastError() != STORM_ERROR_REQUIRES_UPGRADE || !multi_upgrade(a4))) {
+			    && (!i || SErrGetLastError() != STORM_ERROR_REQUIRES_UPGRADE || !multi_upgrade(pfExitProgram))) {
 				return 0;
 			}
 			if (type == 'BNET')
@@ -925,23 +925,25 @@ int __fastcall multi_init_multi(_SNETPROGRAMDATA *client_info, _SNETPLAYERDATA *
 // 678640: using guessed type char byte_678640;
 // 679660: using guessed type char gbMaxPlayers;
 
-int __fastcall multi_upgrade(int *a1)
+BOOL __fastcall multi_upgrade(int *pfExitProgram)
 {
-	int *v1;    // esi
-	int result; // eax
-	int status; // [esp+4h] [ebp-4h]
+	BOOL result;
+	int status;
 
-	v1 = a1;
 	SNetPerformUpgrade((LPDWORD)&status);
-	result = 1;
+	result = TRUE;
 	if (status && status != 1) {
-		if (status == 2) {
-			*v1 = 1;
-		} else if (status == -1) {
-			DrawDlg("Network upgrade failed");
+		if (status != 2) {
+			if (status == -1) {
+				DrawDlg("Network upgrade failed");
+			}
+		} else {
+			*pfExitProgram = 1;
 		}
-		result = 0;
+
+		result = FALSE;
 	}
+
 	return result;
 }
 
