2d3d854813d73fc46cd5e9c5747cd3ca0f48bb8e
aperturesecurity <tatrasiel7@gmail.com>
2019-03-13 21:51:37 +0200

    clean AddHeal

---
+++ orig.asm
@@ -0,0 +1,83 @@
+push ebp
+mov ebp, esp
+push ecx
+push ecx
+push ebx
+push esi
+push edi
+push 0x0A
+mov [ebp-0x08], ecx
+pop edx
+mov cl, 0x39
+call <imm_fn>
+mov esi, [ebp+0x1C]
+mov edi, eax
+imul esi, esi, 0x54D8
+inc edi
+xor ebx, ebx
+shl edi, 0x06
+cmp [esi+0x686600], bl
+jle $+0x1C
+push 0x04
+mov cl, 0x39
+pop edx
+call <imm_fn>
+inc eax
+shl eax, 0x06
+add edi, eax
+inc ebx
+movsx eax, byte ptr [esi+0x686600]
+cmp ebx, eax
+jl $-0x1C
+mov ebx, [ebp-0x08]
+and dword ptr [ebp-0x04], 0x00
+imul ebx, ebx, 0xB0
+cmp dword ptr [ebx+0x647130], 0x00
+jle $+0x1E
+push 0x06
+mov cl, 0x39
+pop edx
+call <imm_fn>
+inc eax
+shl eax, 0x06
+add edi, eax
+inc [ebp-0x04]
+mov eax, [ebp-0x04]
+cmp eax, [ebx+0x647130]
+jl $-0x1E
+mov al, [esi+0x6865A8]
+test al, al
+jnz $+0x2
+add edi, edi
+cmp al, 0x01
+jnz $+0x6
+mov eax, edi
+sar eax, 0x01
+add edi, eax
+mov ecx, [esi+0x6865E4]
+lea eax, [esi+0x6865E0]
+add [eax], edi
+mov edx, [eax]
+cmp edx, ecx
+jle $+0x2
+mov [eax], ecx
+lea eax, [esi+0x6865D8]
+mov esi, [esi+0x6865DC]
+add [eax], edi
+mov ecx, [eax]
+cmp ecx, esi
+jle $+0x2
+mov [eax], esi
+mov ecx, [ebp+0x1C]
+push 0x02
+pop edx
+call <imm_fn>
+xor eax, eax
+inc eax
+pop edi
+mov [ebx+0x647134], eax
+pop esi
+mov [0x4B84D8], eax
+pop ebx
+leave
+ret 0x1C
