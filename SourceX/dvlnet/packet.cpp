2cd5c1409b78e215c5021ec1814ae0839c10b4ef
Xadhoom <>
2019-03-13 09:57:08 +0000

    Ensure libsodium compatibility

--- SourceX/dvlnet/packet.cpp
+++ SourceX/dvlnet/packet.cpp
@@ -161,16 +161,16 @@ packet_factory::packet_factory(std::string pw)
 {
 	if (sodium_init() < 0)
 		ABORT();
-	//pw.resize(std::min(pw.size(), crypto_pwhash_PASSWD_MAX));
-	//pw.resize(std::max(pw.size(), crypto_pwhash_PASSWD_MIN), 0);
+	pw.resize(std::min(pw.size(), crypto_pwhash_argon2id_PASSWD_MAX));
+	pw.resize(std::max(pw.size(), crypto_pwhash_argon2id_PASSWD_MIN), 0);
 	std::string salt("devilution-salt 0.1.0");
-	salt.resize(crypto_pwhash_SALTBYTES, 0);
+	salt.resize(crypto_pwhash_argon2id_SALTBYTES, 0);
 	if (crypto_pwhash(key.data(), crypto_secretbox_KEYBYTES,
 	                  pw.data(), pw.size(),
 	                  reinterpret_cast<const unsigned char *>(salt.data()),
-	                  crypto_pwhash_OPSLIMIT_INTERACTIVE,
-	                  crypto_pwhash_MEMLIMIT_INTERACTIVE,
-	                  crypto_pwhash_ALG_DEFAULT))
+	                  crypto_pwhash_argon2id_OPSLIMIT_INTERACTIVE,
+	                  crypto_pwhash_argon2id_MEMLIMIT_INTERACTIVE,
+	                  crypto_pwhash_ALG_ARGON2ID13))
 		ABORT();
 }
 
