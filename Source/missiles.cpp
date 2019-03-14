a5451fe3d1f038c6a70096ed54f4f62079bbc364
Anders Jenbo <anders@jenbo.dk>
2019-03-14 01:49:36 +0100

    GetMissileVel bin exact suggestion #1
    Co-Authored-By: qndel <stefan551@o2.pl>

--- Source/missiles.cpp
+++ Source/missiles.cpp
@@ -679,7 +679,7 @@ void __fastcall GetMissileVel(int i, int sx, int sy, int dx, int dy, int v)
 	double dxp,dyp,dr;
 
 	if (dx != sx || dy != sy) {
-		dxp = (double)((dx + sy - sx - dy) << 21);
+		dxp = (dx + sy - sx - dy) << 21;
 		dyp = (double)((dy + dx - sx - sy) << 21);
 		dr = sqrt(dxp * dxp + dyp * dyp);
 		missile[i]._mixvel = (dxp * (v << 16)) / dr;
