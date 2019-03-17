f69a927c8b0e5ff72d2e63259ea3b7870acb395d
Benny Franco Dennis <bennyfranco@icloud.com>
2019-03-17 05:37:16 -0700

    Xcode project for macOS x86, build script and travis file update

---
+++ .travis/xcode-build.sh
@@ -0,0 +1,55 @@
+#!/bin/bash
+
+mkdir libs
+
+cd libs
+
+curl -O https://www.libsdl.org/release/SDL2-2.0.9.zip
+curl -O https://www.libsdl.org/projects/SDL_mixer/release/SDL2_mixer-2.0.4.zip
+curl -O https://www.libsdl.org/projects/SDL_ttf/release/SDL2_ttf-2.0.15.zip
+curl -SLO https://download.savannah.gnu.org/releases/freetype/freetype-2.9.1.tar.gz
+curl -SLO https://github.com/glennrp/libpng/archive/v1.6.36.zip
+curl -SLO https://github.com/jedisct1/libsodium/archive/1.0.17.zip
+
+unzip SDL2-2.0.9.zip
+unzip SDL2_mixer-2.0.4.zip
+unzip SDL2_ttf-2.0.15.zip
+unzip v1.6.36.zip
+tar -zxvf freetype-2.9.1.tar.gz
+unzip 1.0.17.zip
+
+xcodebuild -project "SDL2-2.0.9/Xcode/SDL/SDL.xcodeproj" -scheme "Framework" build -configuration Release CONFIGURATION_BUILD_DIR="~/Library/Frameworks" ARCHS="i386 x86_64" ONLY_ACTIVE_ARCH=NO
+xcodebuild -project "SDL2_mixer-2.0.4/Xcode/SDL_mixer.xcodeproj" -scheme "Framework" build -configuration Release ARCHS="i386 x86_64" ONLY_ACTIVE_ARCH=NO
+cp -a SDL2_mixer-2.0.4/Xcode/DerivedData/SDL_mixer/Build/Products/Release/SDL2_mixer.framework ~/Library/Frameworks
+mkdir libpng-1.6.36/build
+cmake -S libpng-1.6.36 -B libpng-1.6.36/build -G"Xcode"
+xcodebuild -project "libpng-1.6.36/build/libpng.xcodeproj" -scheme "ALL_BUILD" build -configuration Release ARCHS="i386 x86_64" ONLY_ACTIVE_ARCH=NO
+sudo mv /usr/local/lib/libpng16.16.dylib /usr/local/lib/libpng16.16_o.dylib
+sudo cp -a libpng-1.6.36/build/Release/libpng16.16.36.0.dylib /usr/local/lib/libpng16.16.dylib
+mkdir freetype-2.9.1/build
+cd freetype-2.9.1
+cmake -S . -B ./build -G"Xcode" -D BUILD_FRAMEWORK:BOOL=true
+cd ..
+xcodebuild -project "freetype-2.9.1/build/freetype.xcodeproj" -scheme "ALL_BUILD" build -configuration Release ARCHS="i386 x86_64" ONLY_ACTIVE_ARCH=NO
+rm -vr SDL2_ttf-2.0.15/Xcode/Frameworks/FreeType.framework
+cp -a freetype-2.9.1/build/Release/freetype.framework SDL2_ttf-2.0.15/Xcode/Frameworks/FreeType.framework
+xcodebuild -project "SDL2_ttf-2.0.15/Xcode/SDL_ttf.xcodeproj" -scheme "Framework" build -configuration Release ARCHS="i386 x86_64" ONLY_ACTIVE_ARCH=NO
+cp -a SDL2_ttf-2.0.15/Xcode/DerivedData/SDL_ttf/Build/Products/Release/SDL2_ttf.framework ~/Library/Frameworks
+
+cp 3rdParty/libsodium/osxi386.sh libs/libsodium-1.0.17/dist-build/osxi386.sh
+cd libsodium-1.0.17
+sudo ./autogen.sh
+./dist-build/osxi386.sh
+sudo cp -a libsodium-osx/lib/ /usr/local/lib/
+sudo cp -a libsodium-osx/include/ /usr/local/include/
+
+cd ../..
+
+xcodebuild -project "./Xcode/devilutionX.xcodeproj" -scheme "devilutionX" build -configuration Release CONFIGURATION_BUILD_DIR="build"
+
+mkdir build/devilutionX
+mv -v build/devilutionX.app build/devilutionX/devilutionX.app
+
+hdiutil create build/devilutionX_temp.dmg -ov -volname "devilutionX" -fs HFS+ -srcfolder "build/devilutionX"
+hdiutil convert build/devilutionX_temp.dmg -format UDZO -o build/devilutionX.dmg
+rm build/devilutionX_temp.dmg
