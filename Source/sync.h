ec2029e0ba7e06374968e277205881ef867fbd72
galaxyhaxz <galaxy-haxz@hotmail.com>
2019-04-04 14:44:37 -0500

    Fix sync.cpp names p1 (#828)

--- Source/sync.h
+++ Source/sync.h
@@ -2,10 +2,10 @@
 #ifndef __SYNC_H__
 #define __SYNC_H__
 
-extern short sync_word_6AA708[MAXMONSTERS];
-extern int syncmonsters; // weak
-extern short sync_word_6AA89C[MAXMONSTERS];
-extern int syncitems;
+extern WORD sync_word_6AA708[MAXMONSTERS];
+extern int sgnMonsters; // weak
+extern WORD sgwLRU[MAXMONSTERS];
+extern int sgnSyncItem;
 
 int __fastcall sync_all_monsters(TSyncHeader *packet, int size);
 void __cdecl sync_one_monster();
@@ -13,8 +13,8 @@ int __fastcall sync_monster_active(TSyncMonster *packet);
 void __fastcall sync_monster_pos(TSyncMonster *packet, int mon_id);
 int __fastcall sync_monster_active2(TSyncMonster *packet);
 void __fastcall SyncPlrInv(TSyncHeader *pSync);
-int __fastcall SyncData(int pnum, TSyncHeader *packet);
-void __fastcall sync_monster_data(int pnum, TSyncMonster *packet);
-void __cdecl sync_clear_pkt();
+int __fastcall sync_update(int pnum, TSyncHeader *packet);
+void __fastcall sync_monster(int pnum, TSyncMonster *packet);
+void __cdecl sync_init();
 
 #endif /* __SYNC_H__ */
