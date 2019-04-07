acb5e25a44457ca8548ece33b8e380b759833558
galaxyhaxz <galaxy-haxz@hotmail.com>
2019-04-07 15:40:43 -0500

    Rename InitPlayerLoc

--- Source/player.h
+++ Source/player.h
@@ -35,7 +35,7 @@ void __fastcall AddPlrExperience(int pnum, int lvl, int exp);
 void __fastcall AddPlrMonstExper(int lvl, int exp, char pmask);
 void __fastcall InitPlayer(int pnum, BOOL FirstTime);
 void __cdecl InitMultiView();
-void __fastcall InitPlayerLoc(int pnum, BOOL flag);
+void __fastcall CheckEFlag(int pnum, BOOL flag);
 BOOL __fastcall SolidLoc(int x, int y);
 BOOL __fastcall PlrDirOK(int pnum, int dir);
 void __fastcall PlrClrTrans(int x, int y);
