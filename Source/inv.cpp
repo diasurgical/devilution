4a99a4a25b3550254e4ca60ffe235893fe418250
Anders Jenbo <anders@jenbo.dk>
2019-04-17 16:21:13 +0200

    Clean up SyncPutItem

--- Source/inv.cpp
+++ Source/inv.cpp
@@ -1990,121 +1990,79 @@ int InvPutItem(int pnum, int x, int y)
 
 int SyncPutItem(int pnum, int x, int y, int idx, WORD icreateinfo, int iseed, int Id, int dur, int mdur, int ch, int mch, int ivalue, unsigned int ibuff)
 {
-	int v13;        // ebx
-	int v14;        // edi
-	int v15;        // esi
-	int v17;        // edi
-	int v18;        // ecx
-	int v19;        // edi
-	int v20;        // eax
-	int v21;        // eax
-	int v22;        // eax
-	int v23;        // edx
-	int v25;        // ecx
-	int *v26;       // edx
-	int v27;        // eax
-	int v28;        // eax
-	int v29;        // [esp+Ch] [ebp-18h]
-	int v30;        // [esp+Ch] [ebp-18h]
-	signed int v31; // [esp+10h] [ebp-14h]
-	int v32;        // [esp+14h] [ebp-10h]
-	int v33;        // [esp+18h] [ebp-Ch]
-	int o1;         // [esp+1Ch] [ebp-8h]
-	signed int v35; // [esp+20h] [ebp-4h]
-	int i;          // [esp+2Ch] [ebp+8h]
-	int ia;         // [esp+2Ch] [ebp+8h]
-	int ib;         // [esp+2Ch] [ebp+8h]
-	int ic;         // [esp+2Ch] [ebp+8h]
+	BOOL done;
+	int d, ii;
+	int i, j, l;
+	int xx, yy;
 
-	v13 = x;
-	v14 = pnum;
 	if (numitems >= 127)
 		return -1;
-	v15 = y;
+
 	if (FindGetItem(idx, icreateinfo, iseed) != -1) {
 		DrawInvMsg("A duplicate item has been detected from another player.");
-		SyncGetItem(v13, y, idx, icreateinfo, iseed);
+		SyncGetItem(x, y, idx, icreateinfo, iseed);
 	}
-	v17 = v14;
-	i = GetDirection(plr[v17].WorldX, plr[v17].WorldY, v13, y);
-	v29 = v15 - plr[v17].WorldY;
-	if (abs(v13 - plr[v17].WorldX) > 1 || abs(v29) > 1) {
-		v13 = plr[v17].WorldX + offset_x[i];
-		v15 = plr[v17].WorldY + offset_y[i];
+
+	d = GetDirection(plr[pnum].WorldX, plr[pnum].WorldY, x, y);
+	xx = x - plr[pnum].WorldX;
+	yy = y - plr[pnum].WorldY;
+	if (abs(xx) > 1 || abs(yy) > 1) {
+		x = plr[pnum].WorldX + offset_x[d];
+		y = plr[pnum].WorldY + offset_y[d];
 	}
-	if (!CanPut(v13, v15)) {
-		v18 = plr[v17].WorldX;
-		v19 = plr[v17].WorldY;
-		v20 = ((_BYTE)i - 1) & 7;
-		v30 = v18;
-		ia = v20;
-		v20 *= 4;
-		v13 = v18 + *(int *)((char *)offset_x + v20);
-		v15 = v19 + *(int *)((char *)offset_y + v20);
-		if (!CanPut(v18 + *(int *)((char *)offset_x + v20), v19 + *(int *)((char *)offset_y + v20))) {
-			v21 = ((_BYTE)ia + 2) & 7;
-			v13 = v30 + offset_x[v21];
-			v15 = v19 + offset_y[v21];
-			if (!CanPut(v30 + offset_x[v21], v19 + offset_y[v21])) {
-				v35 = 0;
-				v22 = -1;
-				ib = 1;
-				v31 = -1;
-				while (!v35) {
-					v33 = v22;
-					while (v22 <= ib && !v35) {
-						v23 = v19 + v33;
-						v32 = v31;
-						o1 = v30 + v31;
-						do {
-							if (v35)
-								break;
-							if (CanPut(o1, v23)) {
-								v13 = o1;
-								v35 = 1;
-								v15 = v23;
+	if (!CanPut(x, y)) {
+		d = (d - 1) & 7;
+		x = plr[pnum].WorldX + offset_x[d];
+		y = plr[pnum].WorldY + offset_y[d];
+		if (!CanPut(x, y)) {
+			d = (d + 2) & 7;
+			x = plr[pnum].WorldX + offset_x[d];
+			y = plr[pnum].WorldY + offset_y[d];
+			if (!CanPut(x, y)) {
+				done = FALSE;
+				for (l = 1; l < 50 && !done; l++) {
+					for (j = -l; j <= l && !done; j++) {
+						yy = j + plr[pnum].WorldY;
+						for (i = -l; i <= l && !done; i++) {
+							xx = i + plr[pnum].WorldX;
+							if (CanPut(xx, yy)) {
+								done = TRUE;
+								x = xx;
+								y = yy;
 							}
-							++v32;
-							++o1;
-						} while (v32 <= ib);
-						v22 = ++v33;
-					}
-					++ib;
-					v22 = v31-- - 1;
-					if (v31 <= -50) {
-						if (v35)
-							break;
-						return -1;
+						}
 					}
 				}
+				if (!done)
+					return -1;
 			}
 		}
 	}
-	CanPut(v13, v15);
-	v25 = itemavail[0];
-	ic = itemavail[0];
-	dItem[v13][v15] = _LOBYTE(itemavail[0]) + 1;
-	v26 = &itemavail[MAXITEMS - numitems - 1];
-	itemactive[numitems] = v25;
-	itemavail[0] = *v26;
+
+	CanPut(x, y);
+
+	ii = itemavail[0];
+	dItem[x][y] = ii + 1;
+	itemavail[0] = itemavail[MAXITEMS - (numitems + 1)];
+	itemactive[numitems] = ii;
+
 	if (idx == IDI_EAR) {
-		RecreateEar(v25, icreateinfo, iseed, Id, dur, mdur, ch, mch, ivalue, ibuff);
+		RecreateEar(ii, icreateinfo, iseed, Id, dur, mdur, ch, mch, ivalue, ibuff);
 	} else {
-		RecreateItem(v25, idx, icreateinfo, iseed, ivalue);
+		RecreateItem(ii, idx, icreateinfo, iseed, ivalue);
 		if (Id)
-			item[ic]._iIdentified = TRUE;
-		v27 = ic;
-		item[v27]._iDurability = dur;
-		item[v27]._iMaxDur = mdur;
-		item[v27]._iCharges = ch;
-		item[v27]._iMaxCharges = mch;
+			item[ii]._iIdentified = TRUE;
+		item[ii]._iDurability = dur;
+		item[ii]._iMaxDur = mdur;
+		item[ii]._iCharges = ch;
+		item[ii]._iMaxCharges = mch;
 	}
-	v28 = ic;
-	item[v28]._ix = v13;
-	item[v28]._iy = v15;
-	RespawnItem(ic, 1);
-	++numitems;
-	return ic;
+
+	item[ii]._ix = x;
+	item[ii]._iy = y;
+	RespawnItem(ii, 1);
+	numitems++;
+	return ii;
 }
 
 int CheckInvHLight()
