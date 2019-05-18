9160d659b373811ea7356783d1bfec80575d034a
Sergey Semushin <predelnik@gmail.com>
2019-05-18 17:38:54 +0300

    Clean up codec_decode.

--- Source/codec.h
+++ Source/codec.h
@@ -2,7 +2,7 @@
 #ifndef __CODEC_H__
 #define __CODEC_H__
 
-int codec_decode(void *pbSrcDst, int size, char *pszPassword);
+int codec_decode(BYTE *pbSrcDst, DWORD size, char *pszPassword);
 void codec_init_key(int unused, char *pszPassword);
 int codec_get_encoded_len(int dwSrcBytes);
 void codec_encode(void *pbSrcDst, int size, int size_64, char *pszPassword);
