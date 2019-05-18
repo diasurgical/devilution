f26332cb0727fb8e36064134ae6441d1b53788ed
Sergey Semushin <predelnik@gmail.com>
2019-05-18 18:03:51 +0300

    Prettify codec_get_encoded_len.

--- Source/codec.h
+++ Source/codec.h
@@ -4,7 +4,7 @@
 
 int codec_decode(BYTE *pbSrcDst, DWORD size, char *pszPassword);
 void codec_init_key(int unused, char *pszPassword);
-int codec_get_encoded_len(int dwSrcBytes);
+DWORD codec_get_encoded_len(DWORD dwSrcBytes);
 void codec_encode(void *pbSrcDst, int size, int size_64, char *pszPassword);
 
 #endif /* __CODEC_H__ */
