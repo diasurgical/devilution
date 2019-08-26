ec90987be02e11414d6cc31ef2bf12ad3d3131a1
Anders Jenbo <anders@jenbo.dk>
2019-08-26 13:29:52 +0200

    Switch to new render from Galaxy

--- Source/render.h
+++ Source/render.h
@@ -1,28 +1,9 @@
-//HEADER_GOES_HERE
 #ifndef __RENDER_H__
 #define __RENDER_H__
 
-void drawTopArchesUpperScreen(BYTE *pBuff);
-void drawBottomArchesUpperScreen(BYTE *pBuff, DWORD *pMask);
-void drawUpperScreen(BYTE *pBuff);
-void drawTopArchesLowerScreen(BYTE *pBuff);
-void drawBottomArchesLowerScreen(BYTE *pBuff, DWORD *pMask);
-void drawLowerScreen(BYTE *pBuff);
+void RenderTile(BYTE *pBuff);
+#define drawUpperScreen(p) RenderTile(p)
+#define drawLowerScreen(p) RenderTile(p)
 void world_draw_black_tile(BYTE *pBuff);
-
-/* rdata */
-
-extern int WorldBoolFlag;
-extern DWORD gdwCurrentMask;
-// extern char world_4B3264;
-extern BYTE *gpCelFrame;
-extern DWORD *gpDrawMask;
-// extern char world_4B326D[16];
-extern DWORD RightMask[32];
-extern DWORD LeftMask[32];
-extern DWORD WallMask[32];
-extern int WorldTbl3x16[48];
-extern int WorldTbl17_1[17];
-extern int WorldTbl17_2[17];
 
 #endif /* __RENDER_H__ */
