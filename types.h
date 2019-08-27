927b6be8f84b3ee80e1fa0b970e1f121e819e96c
Anders Jenbo <anders@jenbo.dk>
2019-08-27 04:00:12 +0200

    Always enable SLEEPFIX
    Fixes #231

--- types.h
+++ types.h
@@ -72,9 +72,9 @@ DEVILUTION_BEGIN_NAMESPACE
 #endif
 
 // If defined, don't fry the CPU [Default -> Undefined]
-#ifdef _DEBUG
+//#ifdef _DEBUG
 #define SLEEPFIX
-#endif
+//#endif
 
 // If defined, fix palette glitch in Windows Vista+ [Default -> Undefined]
 //#define COLORFIX
