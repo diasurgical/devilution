88417dbabb5f39542626a183ee782458a1651d9c
qndel <stefan551@o2.pl>
2019-08-30 18:01:23 +0200

    [hellfire] SetPlrDex bin exact

--- Source/player.cpp
+++ Source/player.cpp
@@ -4084,6 +4084,7 @@ void SetPlrDex(int p, int v)
 	plr[p]._pBaseDex = v;
 	CalcPlrInv(p, TRUE);
 
+#ifndef HELLFIRE
 	if (plr[p]._pClass == PC_ROGUE) {
 		dm = plr[p]._pLevel * (plr[p]._pStrength + plr[p]._pDexterity) / 200;
 	} else {
@@ -4091,6 +4092,7 @@ void SetPlrDex(int p, int v)
 	}
 
 	plr[p]._pDamageMod = dm;
+#endif
 }
 
 void SetPlrVit(int p, int v)
