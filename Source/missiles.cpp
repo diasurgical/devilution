44002747b50ca69090465dfe3e4eb0e1ab9c3446
qndel <stefan551@o2.pl>
2019-09-05 01:54:44 +0200

    MI_Fireball cleanup2

--- Source/missiles.cpp
+++ Source/missiles.cpp
@@ -2877,12 +2877,10 @@ void MI_Fireball(int i)
 			missile[i]._mimfnum = 0;
 			SetMissAnim(i, MFILE_BIGEXP);
 			missile[i]._mirange = missile[i]._miAnimLen - 1;
-		} else {
-			if (missile[i]._mix != missile[i]._miVar1 || missile[i]._miy != missile[i]._miVar2) {
+		} else if (missile[i]._mix != missile[i]._miVar1 || missile[i]._miy != missile[i]._miVar2) {
 				missile[i]._miVar1 = missile[i]._mix;
 				missile[i]._miVar2 = missile[i]._miy;
 				ChangeLight(missile[i]._mlid, missile[i]._miVar1, missile[i]._miVar2, 8);
-			}
 		}
 	}
 
