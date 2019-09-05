35eedb4327037ded144e6ec5649d96d1988cd949
qndel <stefan551@o2.pl>
2019-09-05 02:49:04 +0200

    miss_null_1D cleanup

--- Source/missiles.cpp
+++ Source/missiles.cpp
@@ -1867,9 +1867,9 @@ void AddAcid(int mi, int sx, int sy, int dx, int dy, int midir, char mienemy, in
 void miss_null_1D(int mi, int sx, int sy, int dx, int dy, int midir, char mienemy, int id, int dam)
 {
 	missile[mi]._midam = dam;
-	missile[mi]._mirange = 50;
 	missile[mi]._mixvel = 0;
 	missile[mi]._miyvel = 0;
+	missile[mi]._mirange = 50;
 	missile[mi]._miVar1 = 50 - missile[mi]._miAnimLen;
 	missile[mi]._miVar2 = 0;
 }
