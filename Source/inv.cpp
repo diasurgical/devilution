a1aba3c6fdff1442cae64cea026652f8569a537c
Jimmy-cell <54504810+Jimmy-cell@users.noreply.github.com>
2019-09-07 12:32:55 +0300

    bin exact on UseStaff (#1561)

--- Source/inv.cpp
+++ Source/inv.cpp
@@ -2065,7 +2065,11 @@ BOOL UseStaff()
 {
 	if (pcurs == CURSOR_HAND) {
 		if (plr[myplr].InvBody[INVLOC_HAND_LEFT]._itype != ITYPE_NONE
+#ifdef HELLFIRE
+		    && (plr[myplr].InvBody[INVLOC_HAND_LEFT]._iMiscId == IMISC_STAFF || plr[myplr].InvBody[INVLOC_HAND_LEFT]._iMiscId == IMISC_UNIQUE)
+#else
 		    && plr[myplr].InvBody[INVLOC_HAND_LEFT]._iMiscId == IMISC_STAFF
+#endif
 		    && plr[myplr].InvBody[INVLOC_HAND_LEFT]._iSpell == plr[myplr]._pRSpell
 		    && plr[myplr].InvBody[INVLOC_HAND_LEFT]._iCharges > 0) {
 			return TRUE;
