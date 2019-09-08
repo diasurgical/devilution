c326442f8132d190b84e26757e6bd1495289ce27
Anders Jenbo <anders@jenbo.dk>
2019-09-08 15:05:21 +0200

    [hellfire] PM_DoGotHit

--- Source/player.cpp
+++ Source/player.cpp
@@ -3029,6 +3029,22 @@ BOOL PM_DoGotHit(int pnum)
 		app_fatal("PM_DoGotHit: illegal player %d", pnum);
 	}
 
+#ifdef HELLFIRE
+	if (plr[pnum]._pIFlags & (ISPL_FASTRECOVER | ISPL_FASTERRECOVER | ISPL_FASTESTRECOVER)) {
+		frame = 3;
+		if (plr[pnum]._pIFlags & ISPL_FASTERRECOVER)
+			frame = 4;
+		if (plr[pnum]._pIFlags & ISPL_FASTESTRECOVER)
+			frame = 5;
+		if (plr[pnum]._pVar8 > 1 && plr[pnum]._pVar8 < frame) {
+			plr[pnum]._pVar8 = frame;
+		}
+		if (plr[pnum]._pVar8 > plr[pnum]._pHFrames)
+			plr[pnum]._pVar8 = plr[pnum]._pHFrames;
+	}
+
+	if (plr[pnum]._pVar8 == plr[pnum]._pHFrames) {
+#else
 	frame = plr[pnum]._pAnimFrame;
 	if (plr[pnum]._pIFlags & ISPL_FASTRECOVER && frame == 3) {
 		plr[pnum]._pAnimFrame++;
@@ -3041,6 +3057,7 @@ BOOL PM_DoGotHit(int pnum)
 	}
 
 	if (plr[pnum]._pAnimFrame >= plr[pnum]._pHFrames) {
+#endif
 		StartStand(pnum, plr[pnum]._pdir);
 		ClearPlrPVars(pnum);
 		if (random(3, 4)) {
@@ -3050,6 +3067,9 @@ BOOL PM_DoGotHit(int pnum)
 		return TRUE;
 	}
 
+#ifdef HELLFIRE
+	plr[pnum]._pVar8++;
+#endif
 	return FALSE;
 }
 
