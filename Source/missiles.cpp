550db8460694404fbabce3965afa3a9e6f9c11a4
qndel <stefan551@o2.pl>
2019-09-10 23:23:29 +0200

    Update Source/missiles.cpp

--- Source/missiles.cpp
+++ Source/missiles.cpp
@@ -1209,7 +1209,6 @@ void InitMissiles()
 
 void AddLArrow(int mi, int sx, int sy, int dx, int dy, int midir, char mienemy, int id, int dam)
 {
-
 	if (sx == dx && sy == dy) {
 		dx += XDirAdd[midir];
 		dy += YDirAdd[midir];
