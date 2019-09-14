4dcdca87b48738ff5e6ba84e050854cac583c2f7
Sergey Semushin <predelnik@gmail.com>
2019-09-14 18:02:39 +0300

    hellfire: apply item changes to AllItemsList + add several items to enums.

--- Source/itemdat.h
+++ Source/itemdat.h
@@ -2,7 +2,7 @@
 #ifndef __ITEMDAT_H__
 #define __ITEMDAT_H__
 
-extern ItemDataStruct AllItemsList[157];
+extern ItemDataStruct AllItemsList[];
 extern const PLStruct PL_Prefix[84];
 extern const PLStruct PL_Suffix[96];
 extern const UItemStruct UniqueItemList[91];
