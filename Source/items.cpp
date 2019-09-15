8a18b771dd86a953a45825f18e45346bead31108
qndel <stefan551@o2.pl>
2019-09-15 19:38:11 +0200

    [hellfire] SpawnUnique bin exact

--- Source/items.cpp
+++ Source/items.cpp
@@ -2172,6 +2172,9 @@ void SpawnUnique(int uid, int x, int y)
 {
 	int ii, itype;
 
+#ifdef HELLFIRE
+	int curlv = items_get_currlevel();
+#endif
 	if (numitems >= MAXITEMS)
 		return;
 
@@ -2185,7 +2188,11 @@ void SpawnUnique(int uid, int x, int y)
 		itype++;
 	}
 
+#ifdef HELLFIRE
+	GetItemAttrs(ii, itype, curlv);
+#else
 	GetItemAttrs(ii, itype, currlevel);
+#endif
 	GetUniqueItem(ii, uid);
 	SetupItem(ii);
 	numitems++;
