152573bd57f281b414d05080ae1e5d347c94e28e
qndel <stefan551@o2.pl>
2019-09-25 11:34:33 +0200

    more enums

--- defs.h
+++ defs.h
@@ -69,10 +69,14 @@
 #define NUMLEVELS				25
 #define SMITH_ITEMS				25
 #define SMITH_PREMIUM_ITEMS		15
+#define SMITH_MAX_VALUE			200000
+#define SMITH_MAX_PREMIUM_VALUE 200000
 #else
 #define NUMLEVELS				17
 #define SMITH_ITEMS				20
 #define SMITH_PREMIUM_ITEMS		6
+#define SMITH_MAX_VALUE			140000
+#define SMITH_MAX_PREMIUM_VALUE 140000
 #endif
 
 // from diablo 2 beta
