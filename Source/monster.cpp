9fdacc2857d1b55bc4057130801bceecad1f9011
qndel <stefan551@o2.pl>
2019-09-30 04:27:40 +0200

    [hellfire] MAI_Garbud bin exact

--- Source/monster.cpp
+++ Source/monster.cpp
@@ -4426,7 +4426,11 @@ void MAI_Garbud(int i)
 	MonsterStruct *Monst;
 
 	if ((DWORD)i >= MAXMONSTERS)
+#ifdef HELLFIRE
+		return;
+#else
 		app_fatal("MAI_Garbud: Invalid monster %d", i);
+#endif
 
 	Monst = &monster[i];
 	if (Monst->_mmode != MM_STAND) {
