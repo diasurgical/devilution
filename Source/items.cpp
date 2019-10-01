3c872d28389508858646e8f4fd5d7e6cdb9554de
qndel <stefan551@o2.pl>
2019-10-02 00:40:34 +0200

    gamemenu_save_game / gamemenu_load_game bin exact

--- Source/items.cpp
+++ Source/items.cpp
@@ -2534,6 +2534,19 @@ void RecreateEar(int ii, WORD ic, int iseed, int Id, int dur, int mdur, int ch, 
 }
 
 #ifdef HELLFIRE
+void items_427A72()
+{
+	PkItemStruct id;
+	if (dword_691CB0) {
+		if (CornerItemMaybe.IDidx >= 0) {
+			PackItem(&id, &CornerItemMaybe);
+			SRegSaveData("Hellfire", off_4A5AC4, 0, (BYTE *)&id, 19);
+		} else {
+			SRegSaveData("Hellfire", off_4A5AC4, 0, (BYTE *)(0xDEAFB0B), 1); //TODO: some address should be here, figure out what
+		}
+	}
+}
+
 void items_427ABA(int x, int y)
 {
 	int i, ii;
--- Source/items.h
+++ Source/items.h
@@ -78,6 +78,7 @@ void CreateTypeItem(int x, int y, BOOL onlygood, int itype, int imisc, BOOL send
 void RecreateItem(int ii, int idx, WORD icreateinfo, int iseed, int ivalue);
 void RecreateEar(int ii, WORD ic, int iseed, int Id, int dur, int mdur, int ch, int mch, int ivalue, int ibuff);
 #ifdef HELLFIRE
+void items_427A72();
 void items_427ABA(int x, int y);
 #endif
 void SpawnQuestItem(int itemid, int x, int y, int randarea, int selflag);
