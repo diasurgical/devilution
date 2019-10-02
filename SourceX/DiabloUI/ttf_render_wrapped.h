fd0a23aac3e55a7412d8cc21064f9f622e231562
Gleb Mazovetskiy <glex.spb@gmail.com>
2019-10-02 21:35:13 +0100

    Improve RenderUTF8_Solid_Wrapped comment

--- SourceX/DiabloUI/ttf_render_wrapped.h
+++ SourceX/DiabloUI/ttf_render_wrapped.h
@@ -12,8 +12,12 @@ enum class TextAlignment {
 	END,
 };
 
-// `TTF_RenderUTF8_Solid_Wrapped` is only available in SDL2 2.0.16 (unreleased
-// as of this writing). This is a hacky alternative.
+/**
+ * Renders UTF-8, wrapping lines to avoid exceeding wrapLength, and aligning
+ * according to the `x_align` argument.
+ *
+ * This method is slow. Caching the result is recommended.
+ */
 SDL_Surface *RenderUTF8_Solid_Wrapped(
     TTF_Font *font, const char *text, SDL_Color fg, Uint32 wrapLength, TextAlignment x_align = TextAlignment::BEGIN);
 
