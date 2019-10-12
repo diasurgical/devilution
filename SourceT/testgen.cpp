26d2addb8f0fc6cf5f21d2d24aca4536e4f5082d
Robin Eklind <rnd0x00@gmail.com>
2019-10-12 15:30:09 +0200

    testgen: add table for test cases

--- SourceT/testgen.cpp
+++ SourceT/testgen.cpp
@@ -10,11 +10,27 @@ void write_file(char *path, void *buf, int size);
 void gen_drlg_l1_tests(void) {
 	printf("gen_drlg_l1_tests\n");
 
-	// pre.
-	BYTE in_dlvl = 1;
-	BYTE in_dtype = DTYPE_CATHEDRAL;
-	BYTE in_quest_id = QTYPE_INVALID;
-	DWORD in_seed = 123;
+	typedef struct {
+		// meta.
+		char *dungeon_name;
+		// pre.
+		BYTE dlvl;
+		BYTE dtype;
+		BYTE quest_id;
+		DWORD seed;
+	} Golden;
+
+	// Golden test cases.
+	Golden golden[] = {
+		{
+			"Cathedral",     // dungeon_name
+			1,               // dlvl
+			DTYPE_CATHEDRAL, // dtype
+			QTYPE_INVALID,   // quest_id
+			123              // seed
+		}
+	};
+
 	// extra.
 	int in_entry = 0;
 	BYTE in_max_players = 1; // single
@@ -22,61 +38,65 @@ void gen_drlg_l1_tests(void) {
 	// Load MPQ archives.
 	init_archives();
 
-	// Load level graphics.
-	//*gendung.DType = g.dtype;
-	leveltype = in_dtype;
-	//diablo.LoadLevelGraphics();
-	LoadLvlGFX();
+	for (int i = 0; i < sizeof(golden)/sizeof(Golden); i++) {
+		Golden g = golden[i];
 
-	// Establish pre-conditions.
-	//*gendung.DLvl = g.dlvl;
-	currlevel = in_dlvl;
-	//*multi.MaxPlayers = 1;
-	gbMaxPlayers = in_max_players;
-	//for i := range quests.Quests {
-	//	quests.Quests[i].ID = quests.QuestID(i);
-	//	quests.Quests[i].Active = false;
-	//}
-	for (int quest_id = 0; quest_id < MAXQUESTS; quest_id++) {
-		quests[quest_id]._qtype = quest_id;
-		quests[quest_id]._qactive = false;
+		// Load level graphics.
+		//*gendung.DType = g.dtype;
+		leveltype = g.dtype;
+		//diablo.LoadLevelGraphics();
+		LoadLvlGFX();
+
+		// Establish pre-conditions.
+		//*gendung.DLvl = g.dlvl;
+		currlevel = g.dlvl;
+		//*multi.MaxPlayers = 1;
+		gbMaxPlayers = in_max_players;
+		//for i := range quests.Quests {
+		//	quests.Quests[i].ID = quests.QuestID(i);
+		//	quests.Quests[i].Active = false;
+		//}
+		for (int quest_id = 0; quest_id < MAXQUESTS; quest_id++) {
+			quests[quest_id]._qtype   = quest_id;
+			quests[quest_id]._qactive = false;
+		}
+		//*gendung.IsQuestLevel = false;
+		setlevel = false;
+		//if g.questID != quests.Invalid {
+		//	quests.Quests[g.questID].Active = true;
+		//	quests.Quests[g.questID].DLvl = g.dlvl;
+		//}
+		if (g.quest_id != QTYPE_INVALID) {
+			quests[g.quest_id]._qlevel  = g.dlvl;
+			quests[g.quest_id]._qactive = true;
+		}
+
+		// Generate dungeon based on the given seed.
+		//l1.CreateDungeon(g.seed, 0);
+
+		CreateL5Dungeon(g.seed, in_entry);
+
+		// Dump pre-dungeon tiles.
+		char output_path[MAX_PATH];
+		//sprintf(output_path, "testdata/pre-tiles_%d.bin", g.seed);
+		//write_file(output_path, pdungeon, sizeof(pdungeon));
+
+		// Dump tiles.
+		sprintf(output_path, "testdata/tiles_%d.bin", g.seed);
+		write_file(output_path, dungeon, sizeof(dungeon));
+
+		// Dungeon dungeon pieces.
+		sprintf(output_path, "testdata/dpieces_%d.bin", g.seed);
+		write_file(output_path, dPiece, sizeof(dPiece));
+
+		// Dungeon arches.
+		sprintf(output_path, "testdata/arches_%d.bin", g.seed);
+		write_file(output_path, dArch, sizeof(dArch));
+
+		// Dungeon transparency.
+		sprintf(output_path, "testdata/transparency_%d.bin", g.seed);
+		write_file(output_path, dTransVal, sizeof(dTransVal));
 	}
-	//*gendung.IsQuestLevel = false;
-	setlevel = false;
-	//if g.questID != quests.Invalid {
-	//	quests.Quests[g.questID].Active = true;
-	//	quests.Quests[g.questID].DLvl = g.dlvl;
-	//}
-	if (in_quest_id != QTYPE_INVALID) {
-		quests[in_quest_id]._qlevel = in_dlvl;
-		quests[in_quest_id]._qactive = true;
-	}
-
-	// Generate dungeon based on the given seed.
-	//l1.CreateDungeon(g.seed, 0);
-
-	CreateL5Dungeon(in_seed, in_entry);
-
-	// Dump pre-dungeon tiles.
-	char output_path[MAX_PATH];
-	//sprintf(output_path, "testdata/pre-tiles_%d.bin", in_seed);
-	//write_file(output_path, pdungeon, sizeof(pdungeon));
-
-	// Dump tiles.
-	sprintf(output_path, "testdata/tiles_%d.bin", in_seed);
-	write_file(output_path, dungeon, sizeof(dungeon));
-
-	// Dungeon dungeon pieces.
-	sprintf(output_path, "testdata/dpieces_%d.bin", in_seed);
-	write_file(output_path, dPiece, sizeof(dPiece));
-
-	// Dungeon arches.
-	sprintf(output_path, "testdata/arches_%d.bin", in_seed);
-	write_file(output_path, dArch, sizeof(dArch));
-
-	// Dungeon transparency.
-	sprintf(output_path, "testdata/transparency_%d.bin", in_seed);
-	write_file(output_path, dTransVal, sizeof(dTransVal));
 }
 
 void testgen()
