ca745e2594527afb1c3f7bf9991f59a1269dad84
Mathieu Maret <mathieu.maret@gmail.com>
2019-11-01 00:20:03 +0100

    Use PANEL_X instead of SCREENX+PANEL_LEFT
    Co-Authored-By: Anders Jenbo <anders@jenbo.dk>

--- Source/interfac.cpp
+++ Source/interfac.cpp
@@ -36,11 +36,11 @@ void DrawCutscene()
 	DWORD i;
 
 	lock_buf(1);
-	CelDraw(SCREEN_X + PANEL_LEFT, 480 + SCREEN_Y - 1, (BYTE *)sgpBackCel, 1, 640);
+	CelDraw(PANEL_X, 480 + SCREEN_Y - 1, (BYTE *)sgpBackCel, 1, 640);
 
 	for (i = 0; i < sgdwProgress; i++) {
 		DrawProgress(
-		    BarPos[progress_id][0] + i + SCREEN_X + PANEL_LEFT,
+		    BarPos[progress_id][0] + i + PANEL_X,
 		    BarPos[progress_id][1] + SCREEN_Y,
 		    progress_id);
 	}
