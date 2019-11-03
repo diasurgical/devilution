3bd28cc0c12303925dffce8842f6334f1520f3cf
galaxyhaxz <galaxy-haxz@hotmail.com>
2019-11-03 07:36:03 -0600

    Fix function order [spells+pack.cpp]

--- Source/pack.h
+++ Source/pack.h
@@ -3,10 +3,8 @@
 #define __PACK_H__
 
 void PackPlayer(PkPlayerStruct *pPack, int pnum, BOOL manashield);
-void PackItem(PkItemStruct *id, ItemStruct *is);
 void VerifyGoldSeeds(PlayerStruct *pPlayer);
 void UnPackPlayer(PkPlayerStruct *pPack, int pnum, BOOL killok);
-void UnPackItem(PkItemStruct *is, ItemStruct *id);
 
 /* rdata */
 
--- Source/spells.h
+++ Source/spells.h
@@ -7,7 +7,6 @@ void UseMana(int id, int sn);
 BOOL CheckSpell(int id, int sn, char st, BOOL manaonly);
 void CastSpell(int id, int spl, int sx, int sy, int dx, int dy, int caster, int spllvl);
 void DoResurrect(int pnum, int rid);
-void PlacePlayer(int pnum);
 void DoHealOther(int pnum, int rid);
 
 #endif /* __SPELLS_H__ */
