diasurgical/devilutionX

Initial Nintendo Switch support

3rdParty/StormLib/src/StormPort.h
@@ -249,7 +249,7 @@
 #endif // !PLATFORM_WINDOWS
 
 // 64-bit calls are supplied by "normal" calls on Mac
-#if defined(PLATFORM_MAC) || defined(PLATFORM_HAIKU) || defined(PLATFORM_AMIGA)
+#if defined(PLATFORM_MAC) || defined(PLATFORM_HAIKU) || defined(PLATFORM_AMIGA) || defined(PLATFORM_NX)
   #define stat64  stat
   #define fstat64 fstat
   #define lseek64 lseek

Source/init.cpp
@@ -3,7 +3,9 @@
 #include "../3rdParty/Storm/Source/storm.h"
 #include "../DiabloUI/diabloui.h"
 #include <SDL.h>
+#ifndef SWITCH
 #include <config.h>
+#endif
 
 DEVILUTION_BEGIN_NAMESPACE
 
@@ -115,7 +117,11 @@ HANDLE init_test_access(char *mpq_path, char *mpq_name, char *reg_loc, int flags
 
 	for (int i = 0; i < 2; i++) {
 		snprintf(mpq_path, MAX_PATH, "%s%s", Buffer[i], mpq_name);
+#ifndef SWITCH
 		if (SFileOpenArchive(mpq_path, flags, MPQ_FLAG_READ_ONLY, &archive)) {
+#else
+		if (SFileOpenArchive(mpq_path, flags, 0, &archive)) {
+#endif
 			SFileSetBasePath(Buffer[i]);
 			return archive;
 		}
@@ -126,8 +132,10 @@ HANDLE init_test_access(char *mpq_path, char *mpq_name, char *reg_loc, int flags
 
 void init_get_file_info()
 {
+#ifndef SWITCH
 	snprintf(gszProductName, MAX_PATH, "%s v%s", PROJECT_NAME, PROJECT_VERSION);
 	snprintf(gszVersionNumber, MAX_PATH, "version %s", PROJECT_VERSION);
+#endif
 }
 
 LRESULT __stdcall MainWndProc(HWND hWnd, UINT Msg, WPARAM wParam, LPARAM lParam)

diasurgical/devilution

3rdParty/StormLib/src/StormPort.h
@@ -254,7 +254,7 @@
 #endif // !PLATFORM_WINDOWS
 
 // 64-bit calls are supplied by "normal" calls on Mac
-#if defined(PLATFORM_MAC) || defined(PLATFORM_HAIKU) || defined(PLATFORM_AMIGA)
+#if defined(PLATFORM_MAC) || defined(PLATFORM_HAIKU) || defined(PLATFORM_AMIGA) || defined(PLATFORM_SWITCH)
   #define stat64  stat
   #define fstat64 fstat
   #define lseek64 lseek

Source/init.cpp
@@ -94,7 +94,11 @@ HANDLE init_test_access(char *mpq_path, char *mpq_name, char *reg_loc, int dwPri
 
 	for (int i = 0; i < 2; i++) {
 		snprintf(mpq_path, MAX_PATH, "%s%s", Buffer[i], mpq_name);
+#ifndef __SWITCH__
 		if (SFileOpenArchive(mpq_path, dwPriority, MPQ_FLAG_READ_ONLY, &archive)) {
+#else
+		if (SFileOpenArchive(mpq_path, dwPriority, 0, &archive)) {
+#endif
 			SFileSetBasePath(Buffer[i]);
 			return archive;
 