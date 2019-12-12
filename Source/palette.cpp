e3d2590f364f6516e172a9e88843a953ee132a0d
qndel <stefan551@o2.pl>
2019-12-12 12:44:26 +0100

    Update Source/palette.cpp
    Co-Authored-By: Anders Jenbo <anders@jenbo.dk>

--- Source/palette.cpp
+++ Source/palette.cpp
@@ -30,7 +30,7 @@ void palette_init()
 #ifdef __cplusplus
 	error_code = lpDDInterface->CreatePalette(DDPCAPS_ALLOW256 | DDPCAPS_INITIALIZE | DDPCAPS_8BIT, system_palette, &lpDDPalette, NULL);
 #else
-	error_code = lpDDInterface->lpVtbl->CreatePalette(lpDDInterface, DDPCAPS_ALLOW256 | DDPCAPS_8BIT, system_palette, &lpDDPalette, NULL);
+	error_code = lpDDInterface->lpVtbl->CreatePalette(lpDDInterface, DDPCAPS_ALLOW256 | DDPCAPS_INITIALIZE | DDPCAPS_8BIT, system_palette, &lpDDPalette, NULL);
 #endif
 	if (error_code)
 		ErrDlg(IDD_DIALOG8, error_code, "C:\\Src\\Diablo\\Source\\PALETTE.CPP", 143);
