1534e58350acfe0becdd15231a34d0286c7d1fed
qndel <stefan551@o2.pl>
2019-12-13 09:36:39 +0100

    [hellfire] palette_update_crypt , hive bin exact

--- Source/palette.h
+++ Source/palette.h
@@ -24,6 +24,10 @@ void SetFadeLevel(DWORD fadeval);
 void PaletteFadeIn(int fr);
 void PaletteFadeOut(int fr);
 void palette_update_caves();
+#ifdef HELLFIRE
+void palette_update_crypt();
+void palette_update_hive();
+#endif
 void palette_update_quest_palette(int n);
 BOOL palette_get_colour_cycling();
 BOOL palette_set_color_cycling(BOOL enabled);
