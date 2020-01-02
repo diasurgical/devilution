bfaa16ef992cead6aaa5b5ca113b2a05f0778c74
qndel <stefan551@o2.pl>
2020-01-02 21:14:36 +0100

    [hellfire] SyncOpL1Door bin exact

--- Source/objects.cpp
+++ Source/objects.cpp
@@ -3982,15 +3982,13 @@ void OperateObject(int pnum, int i, BOOL TeleFlag)
 
 void SyncOpL1Door(int pnum, int cmd, int i)
 {
-	BOOL do_sync;
+	DIABOOL do_sync;
 
 	if (pnum == myplr)
 		return;
 
 	do_sync = FALSE;
-	if (cmd == CMD_OPENDOOR) {
-		if (object[i]._oVar4 != 0)
-			return;
+	if (cmd == CMD_OPENDOOR && object[i]._oVar4 == 0) {
 		do_sync = TRUE;
 	}
 	if (cmd == CMD_CLOSEDOOR && object[i]._oVar4 == 1)
