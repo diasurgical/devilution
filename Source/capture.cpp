4ff5c8987a0e1e85fd4b8e919377416f401862b6
Anders Jenbo <anders@jenbo.dk>
2020-01-27 00:34:34 +0100

    Fix screen shots on big-endian

--- Source/capture.cpp
+++ Source/capture.cpp
@@ -16,12 +16,12 @@ static BOOL CaptureHdr(short width, short height, std::ofstream *out)
 	Buffer.Version = 5;
 	Buffer.Encoding = 1;
 	Buffer.BitsPerPixel = 8;
-	Buffer.Xmax = width - 1;
-	Buffer.Ymax = height - 1;
-	Buffer.HDpi = width;
-	Buffer.VDpi = height;
+	Buffer.Xmax = SDL_SwapLE16(width - 1);
+	Buffer.Ymax = SDL_SwapLE16(height - 1);
+	Buffer.HDpi = SDL_SwapLE16(width);
+	Buffer.VDpi = SDL_SwapLE16(height);
 	Buffer.NPlanes = 1;
-	Buffer.BytesPerLine = width;
+	Buffer.BytesPerLine = SDL_SwapLE16(width);
 
 	out->write(reinterpret_cast<const char*>(&Buffer), sizeof(Buffer));
 	return !out->fail();
