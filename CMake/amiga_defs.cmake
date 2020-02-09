426d5c7aab6443567dfbf6024d6f2e720024ff32
Anders Jenbo <anders@jenbo.dk>
2020-02-09 22:59:34 +0100

    Move amiga defines to a separate file

---
+++ CMake/amiga_defs.cmake
@@ -0,0 +1,10 @@
+set(ASAN OFF)
+set(UBSAN OFF)
+set(NONET ON)
+set(USE_SDL1 ON)
+set(SDL1_VIDEO_MODE_BPP 8)
+set(TTF_FONT_PATH \"LiberationSerif-Bold.ttf\")
+# Enable exception suport as they are used in dvlnet code
+set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -fexceptions")
+find_package(Freetype REQUIRED)
+find_package(ZLIB REQUIRED)
