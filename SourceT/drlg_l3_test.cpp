9c35dbb7c82a60be92efba9b17a8ddcc0055c7db
Anders Jenbo <anders@jenbo.dk>
2020-02-10 01:52:10 +0100

    Fully test AddFenceDoors

--- SourceT/drlg_l3_test.cpp
+++ SourceT/drlg_l3_test.cpp
@@ -1,7 +1,8 @@
 #include <gtest/gtest.h>
 #include "devilution.h"
 
-TEST(Drlg_l3, AddFenceDoors) {
+TEST(Drlg_l3, AddFenceDoors_x) {
+	memset(dvl::dungeon, 0, sizeof(dvl::dungeon));
 	dvl::dungeon[5][5] = 7;
 	dvl::dungeon[5 - 1][5] = 130;
 	dvl::dungeon[5 + 1][5] = 152;
@@ -9,7 +10,17 @@ TEST(Drlg_l3, AddFenceDoors) {
 	EXPECT_EQ(dvl::dungeon[5][5], 146);
 }
 
+TEST(Drlg_l3, AddFenceDoors_y) {
+	memset(dvl::dungeon, 0, sizeof(dvl::dungeon));
+	dvl::dungeon[5][5] = 7;
+	dvl::dungeon[5][5 - 1] = 130;
+	dvl::dungeon[5][5 + 1] = 152;
+	dvl::AddFenceDoors();
+	EXPECT_EQ(dvl::dungeon[5][5], 147);
+}
+
 TEST(Drlg_l3, AddFenceDoors_no) {
+	memset(dvl::dungeon, 0, sizeof(dvl::dungeon));
 	dvl::dungeon[5][5] = 7;
 	dvl::dungeon[5 - 1][5] = 130;
 	dvl::dungeon[5 + 1][5] = 153;
