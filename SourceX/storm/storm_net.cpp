a2ab6e1e6f5d79928daf716c1a4cc419033c2d16
Gleb Mazovetskiy <glex.spb@gmail.com>
2020-02-15 15:29:12 +0000

    storm_net: Prevent crashing during crash handling
    An error when creating/joining a game was masked by null pointer
    dereference

--- SourceX/storm/storm_net.cpp
+++ SourceX/storm/storm_net.cpp
@@ -86,6 +86,8 @@ BOOL SNetGetGameInfo(int type, void *dst, unsigned int length, unsigned int *byt
 
 BOOL SNetLeaveGame(int type)
 {
+	if (dvlnet_inst == nullptr)
+		return true;
 	return dvlnet_inst->SNetLeaveGame(type);
 }
 
