3a4c1c495234063f13dd5cef051aa9a00abd9538
Anders Jenbo <anders@jenbo.dk>
2020-09-27 04:25:46 +0200

    Apply more defines to replace magic numbers

--- Source/inv.cpp
+++ Source/inv.cpp
@@ -718,7 +718,7 @@ void CheckInvPaste(int pnum, int mx, int my)
 	sx = icursW28;
 	sy = icursH28;
 	done = FALSE;
-	for (r = 0; r < NUM_XY_SLOTS && !done; r++) {
+	for (r = 0; (DWORD)r < NUM_XY_SLOTS && !done; r++) {
 		if (i >= InvRect[r].X && i < InvRect[r].X + INV_SLOT_SIZE_PX) {
 			if (j >= InvRect[r].Y - INV_SLOT_SIZE_PX - 1 && j < InvRect[r].Y) {
 				done = TRUE;
@@ -1961,7 +1961,7 @@ char CheckInvHLight()
 		}
 	}
 
-	if ((DWORD)r >= 73)
+	if ((DWORD)r >= NUM_XY_SLOTS)
 		return -1;
 
 	rv = -1;
