4e7e3e0b462357bea297dda6bce76040f0070237
Anders Jenbo <anders@jenbo.dk>
2020-10-13 01:35:19 +0200

    0 1 0 0 1 ... 2

--- Source/minitext.cpp
+++ Source/minitext.cpp
@@ -15,7 +15,7 @@ BYTE *pMedTextCels;
 BYTE *pTextBoxCels;
 
 /** Maps from font index to medtexts.cel frame number. */
-const BYTE mfontframe[127] = {
+const BYTE mfontframe[128] = {
 	0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
 	0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
 	0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
@@ -28,7 +28,7 @@ const BYTE mfontframe[127] = {
 	26, 42, 0, 43, 0, 0, 0, 1, 2, 3,
 	4, 5, 6, 7, 8, 9, 10, 11, 12, 13,
 	14, 15, 16, 17, 18, 19, 20, 21, 22, 23,
-	24, 25, 26, 48, 0, 49, 0
+	24, 25, 26, 48, 0, 49, 0, 0
 };
 /**
  * Maps from medtexts.cel frame number to character width. Note, the
