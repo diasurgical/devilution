6aa589a40d4ce017949c39df31fd885a2226cf33
qndel <stefan551@o2.pl>
2020-10-12 06:59:42 +0200

    [hellfire] mainmenu_play_intro

--- Source/mainmenu.cpp
+++ Source/mainmenu.cpp
@@ -165,7 +165,11 @@ BOOL mainmenu_multi_player()
 void mainmenu_play_intro()
 {
 	music_stop();
+#ifdef HELLFIRE
+	play_movie("gendata\\Hellfire.smk", TRUE);
+#else
 	play_movie("gendata\\diablo1.smk", TRUE);
+#endif
 	mainmenu_refresh_music();
 }
 #endif
