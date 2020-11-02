926b2ee2ae779b40106db1d9b9c80fe9ad20f0bb
Anders Jenbo <anders@jenbo.dk>
2020-11-02 02:56:37 +0100

    Cleanup debug.h

--- Source/debug.h
+++ Source/debug.h
@@ -7,8 +7,6 @@
 #define __DEBUG_H__
 
 extern BYTE *pSquareCel;
-extern char dMonsDbg[NUMLEVELS][MAXDUNX][MAXDUNY];
-extern char dFlagDbg[NUMLEVELS][MAXDUNX][MAXDUNY];
 
 void LoadDebugGFX();
 void FreeDebugGFX();
@@ -24,11 +22,9 @@ void GiveGoldCheat();
 void StoresCheat();
 void TakeGoldCheat();
 void MaxSpellsCheat();
-void SetSpellLevelCheat(char spl, int spllvl);
 void SetAllSpellsCheat();
 void PrintDebugPlayer(BOOL bNextPlayer);
 void PrintDebugQuest();
-void PrintDebugMonster(int m);
 void GetDebugMonster();
 void NextDebugMonster();
 #endif
