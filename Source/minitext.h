310dc0b1e7addd4aa40931d63e56f6b84a51cce9
Anders Jenbo <anders@jenbo.dk>
2020-11-03 03:56:44 +0100

    Clean up minitext.h

--- Source/minitext.h
+++ Source/minitext.h
@@ -6,26 +6,12 @@
 #ifndef __MINITEXT_H__
 #define __MINITEXT_H__
 
-extern int qtexty;
-extern const char *qtextptr;
 extern BOOLEAN qtextflag;
-extern BYTE *pMedTextCels;
-extern BYTE *pTextBoxCels;
 
 void FreeQuestText();
 void InitQuestText();
 void InitQTextMsg(int m);
 void DrawQTextBack();
-void PrintQTextChr(int sx, int sy, BYTE *pCelBuff, int nCel);
 void DrawQText();
-
-/* rdata */
-
-extern const BYTE mfontframe[128];
-extern const BYTE mfontkern[56];
-
-/* data */
-
-extern int qscroll_spd_tbl[9];
 
 #endif /* __MINITEXT_H__ */
