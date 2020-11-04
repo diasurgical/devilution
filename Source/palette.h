daaf8652cc60298b393f3b917ea341d8a20c259c
Anders Jenbo <anders@jenbo.dk>
2020-11-04 20:36:41 +0100

    Clean up palette.h

--- Source/palette.h
+++ Source/palette.h
@@ -6,10 +6,7 @@
 #ifndef __PALETTE_H__
 #define __PALETTE_H__
 
-extern PALETTEENTRY logical_palette[256];
 extern PALETTEENTRY system_palette[256];
-extern PALETTEENTRY orig_palette[256];
-extern int gdwPalEntries;
 
 void SaveGamma();
 void palette_init();
@@ -31,15 +28,6 @@ void palette_update_quest_palette(int n);
 #ifndef HELLFIRE
 BOOL palette_get_color_cycling();
 BOOL palette_set_color_cycling(BOOL enabled);
-#endif
-
-/* rdata */
-
-/* data */
-
-extern int gamma_correction;
-#ifndef HELLFIRE
-extern BOOL color_cycling_enabled;
 #endif
 
 #endif /* __PALETTE_H__ */
