54425d768c9ec945236a342cce010f91b5ea1fde
Anders Jenbo <anders@jenbo.dk>
2020-11-09 03:37:02 +0100

    Clean up redner.h

--- Source/render.h
+++ Source/render.h
@@ -6,11 +6,7 @@
 #ifndef __RENDER_H__
 #define __RENDER_H__
 
-void drawTopArchesUpperScreen(BYTE *pBuff);
-void drawBottomArchesUpperScreen(BYTE *pBuff, DWORD *pMask);
 void drawUpperScreen(BYTE *pBuff);
-void drawTopArchesLowerScreen(BYTE *pBuff);
-void drawBottomArchesLowerScreen(BYTE *pBuff, DWORD *pMask);
 void drawLowerScreen(BYTE *pBuff);
 void world_draw_black_tile(BYTE *pBuff);
 
@@ -18,13 +14,10 @@ void world_draw_black_tile(BYTE *pBuff);
 
 extern int WorldBoolFlag;
 extern DWORD gdwCurrentMask;
-// extern char world_4B3264;
 extern BYTE *gpCelFrame;
 extern DWORD *gpDrawMask;
-// extern char world_4B326D[16];
 extern DWORD RightMask[TILE_WIDTH];
 extern DWORD LeftMask[TILE_WIDTH];
-extern DWORD WallMask[TILE_WIDTH];
 extern int WorldTbl3x16[48];
 extern int WorldTbl17_1[17];
 extern int WorldTbl17_2[17];
