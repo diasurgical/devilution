964539cf7d3d0cc2bdf239bc6e48ae780b541e8a
Anders Jenbo <anders@jenbo.dk>
2020-11-09 05:12:21 +0100

    Clean up town.h

--- Source/town.h
+++ Source/town.h
@@ -6,30 +6,12 @@
 #ifndef __TOWN_H__
 #define __TOWN_H__
 
-void town_clear_upper_buf(BYTE *pBuff);
-void town_clear_low_buf(BYTE *pBuff);
-void town_special_lower(BYTE *pBuff, int nCel);
-void town_special_upper(BYTE *pBuff, int nCel);
-void town_draw_clipped_e_flag(BYTE *pBuff, int x, int y, int sx, int sy);
 void town_draw_clipped_town(BYTE *pBuff, int sx, int sy, int dx, int dy, int eflag);
-void town_draw_lower(int x, int y, int sx, int sy, int chunks, int eflag);
-void town_draw_clipped_e_flag_2(BYTE *pBuff, int x, int y, int row, int CelSkip, int sx, int sy);
 void town_draw_clipped_town_2(BYTE *pBuff, int sx, int sy, int row, int CelSkip, int dx, int dy, int eflag);
-void town_draw_lower_2(int x, int y, int sx, int sy, int chunks, int row, int eflag);
-void town_draw_e_flag(BYTE *pBuff, int x, int y, int row, int CelCap, int sx, int sy);
 void town_draw_town_all(BYTE *pBuff, int x, int y, int row, int CelCap, int sx, int sy, int eflag);
-void town_draw_upper(int x, int y, int sx, int sy, int chunks, int row, int eflag);
-void T_DrawGame(int x, int y);
-void T_DrawZoom(int x, int y);
 void T_DrawView(int StartX, int StartY);
-void SetTownMicros();
-void T_FillSector(BYTE *P3Tiles, BYTE *pSector, int xi, int yi, int w, int h);
-void T_FillTile(BYTE *P3Tiles, int xx, int yy, int t);
-void T_Pass3();
 #ifdef HELLFIRE
 void town_4751C6();
-void town_475379();
-void town_47552C();
 void town_475595();
 #endif
 void CreateTown(int entry);
