fb6ee4f37c6bacf823811dc87525e2cba0609a85
Anders Jenbo <anders@jenbo.dk>
2020-11-09 04:08:45 +0100

    Clean up setmaps.h

--- Source/setmaps.h
+++ Source/setmaps.h
@@ -7,20 +7,9 @@
 #define __SETMAPS_H__
 
 int ObjIndex(int x, int y);
-void AddSKingObjs();
-void AddSChamObjs();
-void AddVileObjs();
-void DRLG_SetMapTrans(const char *sFileName);
 void LoadSetMap();
 
 /* rdata */
-extern BYTE SkelKingTrans1[8];
-extern BYTE SkelKingTrans2[8];
-extern BYTE SkelKingTrans3[20];
-extern BYTE SkelKingTrans4[28];
-extern BYTE SkelChamTrans1[20];
-extern BYTE SkelChamTrans2[8];
-extern BYTE SkelChamTrans3[36];
 extern const char *const quest_level_names[];
 
 #endif /* __SETMAPS_H__ */
