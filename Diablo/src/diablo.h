b509c19e1aa7746141eefd514ac300b2d5c36eb9
Chronimal <chronimal@tutanota.com>
2020-11-18 21:06:33 -0800

    VS2019 Support and Directory Restructuring

---
+++ Diablo/src/diablo.h
@@ -0,0 +1,74 @@
+/**
+ * @file diablo.h
+ *
+ * Interface of the main game initialization functions.
+ */
+#ifndef __DIABLO_H__
+#define __DIABLO_H__
+
+extern HWND ghMainWnd;
+extern DWORD glSeedTbl[NUMLEVELS];
+extern int gnLevelTypeTbl[NUMLEVELS];
+extern int MouseX;
+extern int MouseY;
+extern BOOL gbRunGame;
+extern BOOL gbRunGameResult;
+extern BOOL zoomflag;
+extern BOOL gbProcessPlayers;
+extern BOOL gbLoadGame;
+extern HINSTANCE ghInst;
+extern int DebugMonsters[10];
+extern BOOLEAN cineflag;
+extern int force_redraw;
+extern BOOL visiondebug;
+/** unused */
+extern BOOL light4flag;
+extern BOOL leveldebug;
+extern BOOL monstdebug;
+/** unused */
+extern int debugmonsttypes;
+extern int PauseMode;
+#ifdef HELLFIRE
+extern BOOLEAN UseTheoQuest;
+extern BOOLEAN UseCowFarmer;
+extern BOOLEAN UseNestArt;
+extern BOOLEAN UseBardTest;
+extern BOOLEAN UseBarbarianTest;
+extern BOOLEAN UseMultiTest;
+#endif
+extern char sgbMouseDown;
+
+void FreeGameMem();
+BOOL StartGame(BOOL bNewGame, BOOL bSinglePlayer);
+BOOL PressEscKey();
+LRESULT CALLBACK DisableInputWndProc(HWND hWnd, UINT uMsg, WPARAM wParam, LPARAM lParam);
+LRESULT CALLBACK GM_Game(HWND hWnd, UINT uMsg, WPARAM wParam, LPARAM lParam);
+void PressKey(int vkey);
+void LoadGameLevel(BOOL firstflag, int lvldir);
+void game_loop(BOOL bStartup);
+void diablo_color_cyc_logic();
+#ifdef HELLFIRE
+void alloc_plr();
+#endif
+
+/* rdata */
+
+extern BOOL fullscreen;
+#ifdef _DEBUG
+extern int questdebug;
+extern int debug_mode_key_w;
+extern int debug_mode_key_inverted_v;
+extern int debug_mode_dollar_sign;
+extern int debug_mode_key_d;
+extern int debug_mode_key_i;
+extern int dbgplr;
+extern int dbgqst;
+extern int dbgmon;
+extern int frameflag;
+extern int frameend;
+extern int framerate;
+extern int framestart;
+#endif
+extern BOOL FriendlyMode;
+
+#endif /* __DIABLO_H__ */
---
+++ Diablo/src/types.h
@@ -0,0 +1,72 @@
+/**
+ * @file types.h
+ *
+ * Include OS headers and set compiler state.
+ */
+#ifndef _TYPES_H
+#define _TYPES_H
+
+#define WIN32_LEAN_AND_MEAN
+
+#include "res/resource.h"
+
+#include <windows.h>
+#include <mmsystem.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <ddraw.h>
+#include <dsound.h>
+#include <io.h>
+#include <limits.h>
+#include <math.h>
+#include <time.h>
+#include <process.h>
+#include <shlobj.h>
+#include <shellapi.h>
+
+#ifdef __GNUC__
+#include <ctype.h>
+#endif
+
+// tell Visual C++ to shut the hell up
+#ifdef _MSC_VER
+#pragma warning(disable : 4305) // truncation of int
+#pragma warning(disable : 4018) // signed/unsigned mismatch
+#pragma warning(disable : 4700) // used without having been initialized
+#pragma warning(disable : 4244) // conversion loss
+#pragma warning(disable : 4146) // negative unsigned
+#pragma warning(disable : 4996) // deprecation warning
+#pragma warning(disable : 4309) // VC2017: truncation of constant value
+#pragma warning(disable : 4267) // VC2017: conversion from 'size_t' to 'char'
+#pragma warning(disable : 4302) // VC2017: type cast truncation
+#pragma warning(disable : 4334) // VC2017: result of 32-bit shift implicitly converted to 64 bits
+#endif
+
+#include "shared/defs.h"
+#include "shared/enums.h"
+#include "shared/structs.h"
+
+#if (_MSC_VER >= 800) && (_MSC_VER <= 1200)
+#define USE_ASM
+#endif
+
+// If defined, use copy protection [Default -> Defined]
+#if !defined(_DEBUG) && !defined(SPAWN)
+#define COPYPROT
+#endif
+
+// If defined, don't reload for debuggers [Default -> Undefined]
+// Note that with patch 1.03 the command line was hosed, this is required to pass arguments to the game
+#ifdef _DEBUG
+#define DEBUGGER
+#endif
+
+// If defined, don't fry the CPU [Default -> Undefined]
+#ifdef _DEBUG
+#define SLEEPFIX
+#endif
+
+// If defined, fix palette glitch in Windows Vista+ [Default -> Undefined]
+//#define COLORFIX
+
+#endif
