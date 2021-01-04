758b2a65e7fb93ae40bae23480903f90892432a4
Anders Jenbo <anders@jenbo.dk>
2021-01-04 06:23:46 +0100

    Clean up OperateShrine

--- Source/objects.cpp
+++ Source/objects.cpp
@@ -3514,7 +3514,7 @@ void OperateShrine(int pnum, int i, int sType)
 	DWORD lv, t;
 	int xx, yy;
 	int v1, v2, v3, v4;
-	unsigned __int64 spell, spells;
+	unsigned __int64 spell;
 
 	if (dropGoldFlag) {
 		dropGoldFlag = FALSE;
@@ -3534,8 +3534,8 @@ void OperateShrine(int pnum, int i, int sType)
 		object[i]._oAnimFlag = 1;
 		object[i]._oAnimDelay = 1;
 	} else {
-		object[i]._oAnimFlag = 0;
 		object[i]._oAnimFrame = object[i]._oAnimLen;
+		object[i]._oAnimFlag = 0;
 	}
 	switch (object[i]._oVar1) {
 	case SHRINE_MYSTERIOUS:
@@ -3588,10 +3588,12 @@ void OperateShrine(int pnum, int i, int sType)
 			while (TRUE) {
 				cnt = 0;
 				for (j = 0; j < NUM_INVLOC; j++) {
-					if (plr[pnum].InvBody[j]._itype != ITYPE_NONE
-					    && plr[pnum].InvBody[j]._iMaxDur != DUR_INDESTRUCTIBLE
-					    && plr[pnum].InvBody[j]._iMaxDur != 0)
-						cnt++;
+					if (plr[pnum].InvBody[j]._itype != ITYPE_NONE)
+#ifndef HELLFIRE
+						if (plr[pnum].InvBody[j]._iMaxDur != DUR_INDESTRUCTIBLE
+						    && plr[pnum].InvBody[j]._iMaxDur != 0)
+#endif
+							cnt++;
 				}
 				if (cnt == 0)
 					break;
@@ -3649,8 +3651,8 @@ void OperateShrine(int pnum, int i, int sType)
 					plr[pnum].InvList[j]._iMaxDam = plr[pnum].InvList[j]._iMinDam;
 				break;
 			case ITYPE_SHIELD:
-			case ITYPE_LARMOR:
 			case ITYPE_HELM:
+			case ITYPE_LARMOR:
 			case ITYPE_MARMOR:
 			case ITYPE_HARMOR:
 				plr[pnum].InvList[j]._iAC += 2;
@@ -3743,9 +3745,8 @@ void OperateShrine(int pnum, int i, int sType)
 			return;
 		cnt = 0;
 		spell = 1;
-		spells = plr[pnum]._pMemSpells;
 		for (j = 0; j < MAX_SPELLS; j++) {
-			if (spell & spells)
+			if (spell & plr[pnum]._pMemSpells)
 				cnt++;
 			spell <<= 1;
 		}
@@ -3761,10 +3762,10 @@ void OperateShrine(int pnum, int i, int sType)
 			do {
 				r = random_(0, MAX_SPELLS);
 			} while (!(plr[pnum]._pMemSpells & SPELLBIT(r + 1)));
-			if (plr[pnum]._pSplLvl[r] >= 2)
-				plr[pnum]._pSplLvl[r] -= 2;
+			if (plr[pnum]._pSplLvl[r + 1] >= 2)
+				plr[pnum]._pSplLvl[r + 1] -= 2;
 			else
-				plr[pnum]._pSplLvl[r] = 0;
+				plr[pnum]._pSplLvl[r + 1] = 0;
 		}
 		InitDiabloMsg(EMSG_SHRINE_ENCHANTED);
 		break;
@@ -3777,8 +3778,8 @@ void OperateShrine(int pnum, int i, int sType)
 			        || object[v1]._otype == OBJ_CHEST3)
 			    && object[v1]._oSelFlag == 0) {
 				object[v1]._oRndSeed = GetRndSeed();
-				object[v1]._oAnimFrame -= 2;
 				object[v1]._oSelFlag = 1;
+				object[v1]._oAnimFrame -= 2;
 			}
 		}
 		if (deltaload)
@@ -3805,12 +3806,12 @@ void OperateShrine(int pnum, int i, int sType)
 		plr[pnum]._pMaxMana -= t;
 		plr[pnum]._pMaxManaBase -= t;
 		if (plr[pnum]._pMana >> 6 <= 0) {
-			plr[pnum]._pManaBase = 0;
 			plr[pnum]._pMana = v1;
+			plr[pnum]._pManaBase = 0;
 		}
 		if (plr[pnum]._pMaxMana >> 6 <= 0) {
-			plr[pnum]._pMaxManaBase = 0;
 			plr[pnum]._pMaxMana = v2;
+			plr[pnum]._pMaxManaBase = 0;
 		}
 		InitDiabloMsg(EMSG_SHRINE_FASCINATING);
 		break;
@@ -3911,10 +3912,10 @@ void OperateShrine(int pnum, int i, int sType)
 		do {
 			xx = random_(159, MAXDUNX);
 			yy = random_(159, MAXDUNY);
-			j++;
-			if (j > MAXDUNX * 112)
-				break;
 			lv = dPiece[xx][yy];
+			j++;
+			if (j > MAXDUNX * MAXDUNY)
+				break;
 		} while (nSolidTable[lv] || dObject[xx][yy] != 0 || dMonster[xx][yy] != 0);
 		AddMissile(plr[pnum]._px, plr[pnum]._py, xx, yy, plr[pnum]._pdir, MIS_RNDTELEPORT, -1, pnum, 0, 2 * leveltype);
 		if (pnum != myplr)
@@ -4120,6 +4121,9 @@ void OperateShrine(int pnum, int i, int sType)
 		case PC_SORCERER:
 			ModifyPlrMag(myplr, 2);
 			break;
+		case PC_BARBARIAN:
+			ModifyPlrVit(myplr, 2);
+			break;
 		case PC_MONK:
 			ModifyPlrStr(myplr, 1);
 			ModifyPlrDex(myplr, 1);
@@ -4128,11 +4132,8 @@ void OperateShrine(int pnum, int i, int sType)
 			ModifyPlrDex(myplr, 1);
 			ModifyPlrMag(myplr, 1);
 			break;
-		case PC_BARBARIAN:
-			ModifyPlrVit(myplr, 2);
-			break;
 		}
-		CheckStats(myplr);
+		CheckStats(pnum);
 		AddMissile(
 		    object[i]._ox,
 		    object[i]._oy,
@@ -4158,24 +4159,25 @@ void OperateShrine(int pnum, int i, int sType)
 			magicGain = 5;
 			xpLoss = (signed __int64)((double)playerXP * 0.95);
 		} else {
-			xpLoss = 0;
 			magicGain = playerXP / 1000;
+			xpLoss = 0;
 		}
 		ModifyPlrMag(myplr, magicGain);
 		plr[myplr]._pExperience = xpLoss;
-		CheckStats(myplr);
+		CheckStats(pnum);
 	} break;
 
-	case SHRINE_MENDICANT:
+	case SHRINE_MENDICANT: {
 		if (deltaload)
 			return;
 		if (pnum != myplr)
 			return;
 		InitDiabloMsg(EMSG_SHRINE_MENDICANT);
-		AddPlrExperience(myplr, plr[myplr]._pLevel, plr[myplr]._pGold / 2);
-		TakePlrsMoney(plr[myplr]._pGold / 2);
-		CheckStats(myplr);
-		break;
+		int gold = plr[myplr]._pGold / 2;
+		AddPlrExperience(myplr, plr[myplr]._pLevel, gold);
+		TakePlrsMoney(gold);
+		CheckStats(pnum);
+	} break;
 	case SHRINE_SPARKLING:
 		if (deltaload)
 			return;
@@ -4194,7 +4196,7 @@ void OperateShrine(int pnum, int i, int sType)
 		    0,
 		    3 * currlevel + 2,
 		    0);
-		CheckStats(myplr);
+		CheckStats(pnum);
 		break;
 	case SHRINE_TOWN:
 		if (deltaload)
@@ -4220,8 +4222,8 @@ void OperateShrine(int pnum, int i, int sType)
 		if (pnum != myplr)
 			return;
 		InitDiabloMsg(EMSG_SHRINE_SHIMMERING);
-		plr[myplr]._pMana = plr[myplr]._pMaxMana;
-		plr[myplr]._pManaBase = plr[myplr]._pMaxManaBase;
+		plr[pnum]._pMana = plr[pnum]._pMaxMana;
+		plr[pnum]._pManaBase = plr[pnum]._pMaxManaBase;
 		break;
 
 	case SHRINE_SOLAR: {
@@ -4234,21 +4236,17 @@ void OperateShrine(int pnum, int i, int sType)
 		if (hour > 20 || hour < 4) {
 			InitDiabloMsg(EMSG_SHRINE_SOLAR4);
 			ModifyPlrVit(myplr, 2);
-		} else if (hour <= 18) {
-			if (hour <= 12) {
-				if (hour > 4) {
-					InitDiabloMsg(EMSG_SHRINE_SOLAR1);
-					ModifyPlrDex(myplr, 2);
-				}
-			} else {
-				InitDiabloMsg(EMSG_SHRINE_SOLAR2);
-				ModifyPlrStr(myplr, 2);
-			}
-		} else {
+		} else if (hour > 18) {
 			InitDiabloMsg(EMSG_SHRINE_SOLAR3);
 			ModifyPlrMag(myplr, 2);
+		} else if (hour > 12) {
+			InitDiabloMsg(EMSG_SHRINE_SOLAR2);
+			ModifyPlrStr(myplr, 2);
+		} else if (hour > 4) {
+			InitDiabloMsg(EMSG_SHRINE_SOLAR1);
+			ModifyPlrDex(myplr, 2);
 		}
-		CheckStats(myplr);
+		CheckStats(pnum);
 	} break;
 
 	case SHRINE_MURPHYS:
@@ -4257,12 +4255,13 @@ void OperateShrine(int pnum, int i, int sType)
 		if (pnum != myplr)
 			return;
 		InitDiabloMsg(EMSG_SHRINE_MURPHYS);
-		BOOLEAN broke = FALSE;
+		BOOL broke = FALSE;
 		for (int j = 0; j < NUM_INVLOC; j++) {
-			if (plr[pnum].InvBody[j]._itype != ITYPE_NONE && random_(0, 3) == 0) {
-				if (plr[pnum].InvBody[j]._iDurability != DUR_INDESTRUCTIBLE) {
-					if (plr[pnum].InvBody[j]._iDurability) {
-						plr[pnum].InvBody[j]._iDurability /= 2;
+			ItemStruct *item = &plr[myplr].InvBody[j];
+			if (item->_itype != ITYPE_NONE && random_(0, 3) == 0) {
+				if (item->_iDurability != DUR_INDESTRUCTIBLE) {
+					if (item->_iDurability) {
+						item->_iDurability /= 2;
 						broke = TRUE;
 						break;
 					}
